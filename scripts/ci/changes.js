f2825039c6bebddbfa662688d0595de40db4baa7
Philipp Hugenroth <philipph@spotify.com>
2024-02-20 11:35:16 +0100

    Adjust to workflow syntax
    Signed-off-by: Philipp Hugenroth <philipph@spotify.com>

--- scripts/ci/changes.js
+++ scripts/ci/changes.js
@@ -1,28 +1,14 @@
-import { GitUtils, PackageGraph } from "@backstage/cli-node";
-import { getPackages } from "@manypkg/get-packages";
-import chalk from "chalk";
-import fs from "fs/promises";
+import { GitUtils } from "@backstage/cli-node";
 
-export async function changedWorkspaces() {
-  const changedFiles = await GitUtils.listChangedFiles("main");
-  const workspaces = new Set();
+const changedFiles = await GitUtils.listChangedFiles("main");
+const workspaces = new Set();
 
-  changedFiles.forEach((name) => {
-    // First match with prefix = "workspaces/" & suffix = "/"
-    const match = name.match("(?<=workspaces/)([^/]+)(?=/)");
-    if (match && match[0]) {
-      workspaces.add(match[0]);
-    }
-  });
-  console.log("changed files", workspaces);
-  // backstage-cli repo lint --since origin/master
-
-  for (let workspace in workspaces) {
-    const output = execSync(
-      `npx --yes @backstage/create-app --path ${workspacePath} --skip-install`,
-      { input: answers.name }
-    );
+changedFiles.forEach((name) => {
+  // First match with prefix = "workspaces/" & suffix = "/"
+  const match = name.match("(?<=workspaces/)([^/]+)(?=/)");
+  if (match && match[0]) {
+    workspaces.add(match[0]);
   }
-}
+});
 
-changedWorkspaces();
+console.log(`out=${workspaces.values().next().value}`);
