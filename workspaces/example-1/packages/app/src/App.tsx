0497c5327c063b50eb9345aa7006d2d9b8fe4d13
Vincenzo Scamporlino <vincenzos@spotify.com>
2024-02-26 16:40:31 +0100

    example1: fix import
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- workspaces/example-1/packages/app/src/App.tsx
+++ workspaces/example-1/packages/app/src/App.tsx
@@ -33,7 +33,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
-import { Frontend1Page } from '@internal/example-plugin-frontend-1';
+import { Frontend1Page } from '@backstage/example-plugin-frontend-1';
 
 const app = createApp({
   apis,
