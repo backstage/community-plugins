46ba8eaa1135cd1b9dafc3ee8d30c5c755e2813e
Vincenzo Scamporlino <vincenzos@spotify.com>
2024-02-27 11:29:04 +0100

    Revert "make it fail on purpose"
    This reverts commit 420aa5ae41f918c7233f8feec73280730d767142.
    
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- workspaces/example-1/plugins/frontend-1/src/plugin.test.ts
+++ workspaces/example-1/plugins/frontend-1/src/plugin.test.ts
@@ -2,6 +2,6 @@ import { frontend1Plugin } from './plugin';
 
 describe('frontend-1', () => {
   it('should export plugin', () => {
-    expect(frontend1Plugin).not.toBeDefined();
+    expect(frontend1Plugin).toBeDefined();
   });
 });
