ea39a2244cdca2e0d434ca07255eb81b7dfff6a0
blam <ben@blam.sh>
2024-04-09 11:39:03 +0200

    chore: add some packages   e!
    Signed-off-by: blam <ben@blam.sh>

--- workspaces/repo-tools/packages/cli/src/index.ts
+++ workspaces/repo-tools/packages/cli/src/index.ts
@@ -1,1 +1,30 @@
-console.log('here!');
+import { program } from 'commander';
+import chalk from 'chalk';
+import { exitWithError } from './lib/errors';
+import { registerCommands } from './commands';
+
+const main = (argv: string[]) => {
+  program.name('community-cli')
+
+  registerCommands(program);
+
+  program.on('command:*', () => {
+    console.log();
+    console.log(chalk.red(`Invalid command: ${program.args.join(' ')}`));
+    console.log();
+    program.outputHelp();
+    process.exit(1);
+  });
+
+  program.parse(argv);
+};
+
+process.on('unhandledRejection', rejection => {
+  if (rejection instanceof Error) {
+    exitWithError(rejection);
+  } else {
+    exitWithError(new Error(`Unknown rejection: '${rejection}'`));
+  }
+});
+
+main(process.argv);
