530be58abcd4157ed8d39a751d3806d43997c922
blam <ben@blam.sh>
2024-04-09 16:08:29 +0200

    chore: updating Signed-off-by: blam <ben@blam.sh>
    Signed-off-by: blam <ben@blam.sh>

--- workspaces/repo-tools/packages/cli/src/commands/plugin/migrate.ts
+++ workspaces/repo-tools/packages/cli/src/commands/plugin/migrate.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { OptionValues } from 'commander';
 
 export default async (opts: OptionValues) => {
@@ -6,5 +7,17 @@ export default async (opts: OptionValues) => {
     pluginName: string;
   };
 
-  console.log(`Migrating plugin ${pluginName} in monorepo at ${monorepoPath}`);
+  console.log(
+    chalk.blue`Migrating plugin ${pluginName} in monorepo at ${monorepoPath}`,
+  );
+
+  // check if the workspace exists, create if not.
+
+  // find all the packages in the main monorepo that should be in the workspace
+
+  // copy packages across, and adjust the package.jsons
+
+  // mark copied packages as deprecated and add changeset.
+
+  console.log();
 };
