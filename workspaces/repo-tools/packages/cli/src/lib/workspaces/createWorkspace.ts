4b22b86032ef1e49e7058f8fcd5d902eccdd55ff
blam <ben@blam.sh>
2024-04-10 17:30:17 +0200

    chore: use the old monorepo layout for migrated packages
    Signed-off-by: blam <ben@blam.sh>

--- workspaces/repo-tools/packages/cli/src/lib/workspaces/createWorkspace.ts
+++ workspaces/repo-tools/packages/cli/src/lib/workspaces/createWorkspace.ts
@@ -1,8 +1,9 @@
 import { join } from 'path';
-import { readJson, writeJson, copy, remove, rename } from 'fs-extra';
+import { readJson, writeJson, copy } from 'fs-extra';
 import { execSync } from 'child_process';
 
-// TODO: think we should probably replace this with our own skeleton and not use create-app.
+// TODO: might be worth shipping our our template for create-app, especially if we decide
+// to change the layout of the workspaces moving forward.
 export const createWorkspace = async (opts: { name: string; cwd?: string }) => {
   const workspacePath = join(
     opts.cwd ?? process.cwd(),
@@ -24,14 +25,6 @@ export const createWorkspace = async (opts: { name: string; cwd?: string }) => {
   ];
 
   workspacePackageJson.devDependencies ??= {};
-  workspacePackageJson.workspaces.packages = [
-    'example-app/*',
-    'node',
-    'backend',
-    'frontend',
-    'react',
-    'common',
-  ];
 
   for (const additionalDependency of additionalDevDependencies) {
     const version = execSync(
@@ -45,25 +38,6 @@ export const createWorkspace = async (opts: { name: string; cwd?: string }) => {
   workspacePackageJson.name = opts.name;
 
   await writeJson(join(workspacePath, 'package.json'), workspacePackageJson, {
-    spaces: 2,
-  });
-
-  // Move the current packages directory to example app
-  await rename(
-    join(workspacePath, 'packages'),
-    join(workspacePath, 'example-app'),
-  );
-
-  // Remove the plugins directory
-  await remove(join(workspacePath, 'plugins'));
-  await remove(join(workspacePath, 'lerna.json'));
-  await remove(join(workspacePath, 'examples'));
-
-  const tsConfigJson = await readJson(join(workspacePath, 'tsconfig.json'));
-
-  tsConfigJson.include = ['*/src'];
-
-  await writeJson(join(workspacePath, 'tsconfig.json'), tsConfigJson, {
     spaces: 2,
   });
 
