90080279caf228cb0547bd2b053ded850a190641
blam <ben@blam.sh>
2024-04-18 17:04:20 +0200

    chore: added some more updates for workspace generation
    Signed-off-by: blam <ben@blam.sh>

--- workspaces/repo-tools/packages/cli/src/lib/workspaces/createWorkspace.ts
+++ workspaces/repo-tools/packages/cli/src/lib/workspaces/createWorkspace.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import { copy } from 'fs-extra';
+import { copy, readJson, writeJson } from 'fs-extra';
 import { execSync } from 'child_process';
 
 // TODO: might be worth shipping our our template for create-app, especially if we decide
