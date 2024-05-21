ece895bf94cec140ac597ba6982e59fb0fe9f9d8
blam <ben@blam.sh>
2024-05-21 17:02:52 +0200

    feat: support direct communication to control plane Signed-off-by: blam <ben@blam.sh>

--- workspaces/linkerd/plugins/linkerd-backend/src/service/router.ts
+++ workspaces/linkerd/plugins/linkerd-backend/src/service/router.ts
@@ -4,6 +4,7 @@ import {
   DiscoveryService,
   HttpAuthService,
   LoggerService,
+  RootConfigService,
 } from '@backstage/backend-plugin-api';
 import express from 'express';
 import Router from 'express-promise-router';
@@ -13,17 +14,19 @@ export interface RouterOptions {
   logger: LoggerService;
   discovery: DiscoveryService;
   auth: AuthService;
+  config: RootConfigService;
   httpAuth: HttpAuthService;
 }
 
 export async function createRouter(
   opts: RouterOptions,
 ): Promise<express.Router> {
-  const { discovery, auth, httpAuth } = opts;
+  const { discovery, auth, httpAuth, config } = opts;
 
   const linkerdVizClient = LinkerdVizClient.fromConfig({
     discovery,
     auth,
+    config,
   });
 
   const router = Router();
@@ -39,45 +42,48 @@ export async function createRouter(
       const {
         params: { namespace, deployment },
       } = request;
+      try {
+        const [current] = await linkerdVizClient.stats(
+          { resourceType: 'deployment', namespace, resourceName: deployment },
+          { credentials: await httpAuth.credentials(request) },
+        );
 
-      const [current] = await linkerdVizClient.stats(
-        { resourceType: 'deployment', namespace, resourceName: deployment },
-        { credentials: await httpAuth.credentials(request) },
-      );
+        const incoming = await linkerdVizClient.stats(
+          {
+            allNamespaces: true,
+            toName: deployment,
+            toNamespace: namespace,
+            toType: 'deployment',
+            resourceType: 'all',
+          },
+          { credentials: await httpAuth.credentials(request) },
+        );
 
-      const incoming = await linkerdVizClient.stats(
-        {
-          allNamespaces: true,
-          toName: deployment,
-          toNamespace: namespace,
-          toType: 'deployment',
-          resourceType: 'all',
-        },
-        { credentials: await httpAuth.credentials(request) },
-      );
+        const outgoing = await linkerdVizClient.stats(
+          {
+            allNamespaces: true,
+            fromName: deployment,
+            fromNamespace: namespace,
+            fromType: 'deployment',
+            resourceType: 'all',
+          },
+          { credentials: await httpAuth.credentials(request) },
+        );
 
-      const outgoing = await linkerdVizClient.stats(
-        {
-          allNamespaces: true,
-          fromName: deployment,
-          fromNamespace: namespace,
-          fromType: 'deployment',
-          resourceType: 'all',
-        },
-        { credentials: await httpAuth.credentials(request) },
-      );
+        const edges = await linkerdVizClient.edges(
+          { namespace, resourceType: 'deployment' },
+          { credentials: await httpAuth.credentials(request) },
+        );
 
-      const edges = await linkerdVizClient.edges(
-        { namespace, resourceType: 'deployment' },
-        { credentials: await httpAuth.credentials(request) },
-      );
-
-      response.send({
-        current,
-        incoming,
-        outgoing,
-        edges,
-      });
+        response.send({
+          current,
+          incoming,
+          outgoing,
+          edges,
+        });
+      } catch (ex) {
+        console.log(await ex, ex.stack);
+      }
     },
   );
 
