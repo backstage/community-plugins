1d30ab99c938ee17c37dd338a7008b172cd7cb16
Raghunandan Balachandran <raghunandan@spotify.com>
2024-06-03 13:37:23 +0200

    [Analytics] GA4 filter out custom dimensions with empty string value (#520)
    * Custom dimensions with empty string values should not be passed to GA
    
    Signed-off-by: Raghunandan Balachandran <raghunandan@spotify.com>
    
    * add test to check for empty string values
    
    Signed-off-by: Raghunandan Balachandran <raghunandan@spotify.com>
    
    * prettier fix
    
    Signed-off-by: Raghunandan Balachandran <raghunandan@spotify.com>
    
    ---------
    
    Signed-off-by: Raghunandan Balachandran <raghunandan@spotify.com>

---
+++ workspaces/analytics/.changeset/gorgeous-doors-jam.md
@@ -0,0 +1,5 @@
+---
+'@backstage-community/plugin-analytics-module-ga4': patch
+---
+
+Filter out custom dimenstion keys with empty string values
--- workspaces/analytics/plugins/analytics-module-ga4/src/apis/implementations/AnalyticsApi/GoogleAnalytics4.test.ts
+++ workspaces/analytics/plugins/analytics-module-ga4/src/apis/implementations/AnalyticsApi/GoogleAnalytics4.test.ts
@@ -263,6 +263,35 @@ describe('GoogleAnalytics4', () => {
       });
     });
 
+    it('filters out custom dimenstions with empty string value', () => {
+      const api = GoogleAnalytics4.fromConfig(advancedConfig);
+
+      const expectedAction = 'search';
+      const expectedLabel = 'some query';
+      const expectedValue = 5;
+      api.captureEvent({
+        action: expectedAction,
+        subject: expectedLabel,
+        value: expectedValue,
+        attributes: {
+          extraDimension: '',
+          extraMetric: 0,
+        },
+        context,
+      });
+
+      expect(fnEvent).toHaveBeenCalledWith('search', {
+        action: 'search',
+        category: context.extension,
+        label: expectedLabel,
+        value: expectedValue,
+        c_pluginId: context.pluginId,
+        c_releaseNum: context.releaseNum,
+        a_extraMetric: 0,
+        search_term: expectedLabel,
+      });
+    });
+
     it('does not pass non-numeric data on metrics', () => {
       const api = GoogleAnalytics4.fromConfig(advancedConfig);
 
