950b0e6592f598408a13e65dcfd8c5a7cd2a02c5
Paul Schultz <pschultz@pobox.com>
2024-07-16 15:01:10 -0500

    refactor field extension
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- workspaces/odo/plugins/odo-module-devfile-field-extension/src/plugin.ts
+++ workspaces/odo/plugins/odo-module-devfile-field-extension/src/plugin.ts
@@ -3,16 +3,17 @@ import { createPlugin } from '@backstage/core-plugin-api';
 import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { createScaffolderFieldExtension } from '@backstage/plugin-scaffolder-react';
 
-import { DevfileSelectorExtension, DevfileSelectorExtensionWithOptionsSchema } from './components/DevfileSelectorExtension';
+import { DevfileSelectorExtension } from './components/DevfileSelectorExtension';
+import { DevfileSelectorExtensionWithOptionsSchema } from './components/schema';
 
 export const devfileSelectorExtensionPlugin = createPlugin({
-  id: 'devfile-selector-extension'
-})
+  id: 'devfile-selector-extension',
+});
 
 export const DevfileSelectorFieldExtension = scaffolderPlugin.provide(
   createScaffolderFieldExtension({
-      name: 'DevfileSelectorExtension',
-      component: DevfileSelectorExtension,
-      schema: DevfileSelectorExtensionWithOptionsSchema,
+    name: 'DevfileSelectorExtension',
+    component: DevfileSelectorExtension,
+    schema: DevfileSelectorExtensionWithOptionsSchema,
   }),
 );
