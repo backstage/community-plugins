817c94b9e6fc5a427de98ead5e68521bc9234d27
Alex McKay <amckay@spotify.com>
2024-07-18 15:12:55 -0400

    add example app (legacy FE) & backend (new backend system) packages for github-actions
    Signed-off-by: Alex McKay <amckay@spotify.com>

---
+++ workspaces/github-actions/packages/backend/src/modules/auth/index.ts
@@ -0,0 +1,30 @@
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { githubAuthenticator } from '@backstage/plugin-auth-backend-module-github-provider';
+import {
+  authProvidersExtensionPoint,
+  createOAuthProviderFactory,
+} from '@backstage/plugin-auth-node';
+import { customGithubSignInResolver } from './github';
+
+export default createBackendModule({
+  pluginId: 'auth',
+  moduleId: '@spotify/auth-module',
+  register(reg) {
+    reg.registerInit({
+      deps: {
+        providers: authProvidersExtensionPoint,
+      },
+      async init({ providers }) {
+        providers.registerProvider({
+          providerId: 'github',
+          factory: createOAuthProviderFactory({
+            authenticator: githubAuthenticator,
+            signInResolverFactories: {
+              customGithubSignInResolver,
+            },
+          }),
+        });
+      },
+    });
+  },
+});
