6e6fdf16bc6b848006c392255e9c792c67c22e3b
Alex McKay <amckay@spotify.com>
2024-07-19 09:45:33 -0400

    remove now uneeded deps
    Signed-off-by: Alex McKay <amckay@spotify.com>

--- workspaces/github-actions/packages/app-next/src/components/Sidebar.tsx
+++ workspaces/github-actions/packages/app-next/src/components/Sidebar.tsx
@@ -10,17 +10,13 @@ import {
   coreExtensionData,
   createExtension,
 } from '@backstage/frontend-plugin-api';
-import { SidebarSearchModal } from '@backstage/plugin-search';
 import {
   Settings,
   UserSettingsSignInAvatar,
 } from '@backstage/plugin-user-settings';
-import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
 import ExtensionIcon from '@material-ui/icons/Extension';
 import HomeIcon from '@material-ui/icons/Home';
-import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
 export const navigationExtension = createExtension({
@@ -34,20 +30,11 @@ export const navigationExtension = createExtension({
     return {
       element: compatWrapper(
         <Sidebar>
-          <SidebarGroup label="Search" icon={<SearchIcon />} to="/search">
-            <SidebarSearchModal />
-          </SidebarGroup>
           <SidebarDivider />
           <SidebarGroup label="Menu" icon={<MenuIcon />}>
             {/* Global nav, not org-specific */}
             <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
             <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
-            <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-            <SidebarItem
-              icon={CreateComponentIcon}
-              to="create"
-              text="Create..."
-            />
             {/* End global nav */}
             <SidebarDivider />
           </SidebarGroup>
