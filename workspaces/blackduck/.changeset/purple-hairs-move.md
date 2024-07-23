ee5109a630a5c172a6b4fec7396b93a197f3c75e
Deepankumar Loganathan <deepan0433@gmail.com>
2024-07-23 11:00:04 +0200

    Changeset updated
    Signed-off-by: Deepankumar Loganathan <deepan0433@gmail.com>

---
+++ workspaces/blackduck/.changeset/purple-hairs-move.md
@@ -0,0 +1,7 @@
+---
+'@backstage-community/plugin-blackduck-backend': patch
+'@backstage-community/plugin-blackduck-common': patch
+'@backstage-community/plugin-blackduck': patch
+---
+
+BlackDuck plugin migrated to @backstage-community plugins
--- workspaces/blackduck/plugins/blackduck-backend/src/service/router.test.ts
+++ workspaces/blackduck/plugins/blackduck-backend/src/service/router.test.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import request from 'supertest';
 
 import { createRouter } from './router';
+import { ConfigReader } from '@backstage/config';
 
 describe('createRouter', () => {
   let app: express.Express;
@@ -10,7 +11,12 @@ describe('createRouter', () => {
   beforeAll(async () => {
     const router = await createRouter({
       logger: mockServices.logger.mock(),
-      config: mockServices.rootConfig(),
+      config: new ConfigReader({
+        blackduck: {
+          host: 'blackduck.mycompany.com/api',
+          token: 'foo-token',
+        },
+      }),
       permissions: mockServices.permissions.mock(),
       discovery: mockServices.discovery.mock(),
     });
