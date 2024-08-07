afee253f01c0820b0893cd75e11cfa46830554ba
Florian JUDITH <florian.judith@alithya.com>
2024-08-07 10:30:57 -0400

    Fixed frontend plugin api report
    Signed-off-by: Florian JUDITH <florian.judith@alithya.com>
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- workspaces/confluence/plugins/confluence/src/plugin.ts
+++ workspaces/confluence/plugins/confluence/src/plugin.ts
@@ -1,5 +1,10 @@
 import { createPlugin } from '@backstage/core-plugin-api';
 
+/**
+ * The Backstage plugin that holds Confluence specific components
+ * 
+ * @public
+ */
 export const confluencePlugin = createPlugin({
   id: 'confluence',
 });
