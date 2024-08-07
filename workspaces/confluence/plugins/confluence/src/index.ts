2f7a15a5821c31e34839e87b4dc3b06568c98093
Florian JUDITH <florian.judith@alithya.com>
2024-08-07 10:30:57 -0400

    Fixed frontend plugin api report
    Signed-off-by: Florian JUDITH <florian.judith@alithya.com>

--- workspaces/confluence/plugins/confluence/src/index.ts
+++ workspaces/confluence/plugins/confluence/src/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Confluence frontend plugin.
+ *
+ * @packageDocumentation
+ */
+
 export { confluencePlugin } from './plugin';
 export { ConfluenceSearchIcon } from './icons';
 export { ConfluenceSearchResultListItem } from './search/ConfluenceSearchResultListItem';
+export type { ConfluenceResultItemProps } from './search/ConfluenceSearchResultListItem';
