15738a02308d20aae42e948eb7153aec49b2e1ed
Phillip Morton <Phiph@users.noreply.github.com>
2024-08-14 22:27:11 +0000

    init pipeline scaffolder
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>

---
+++ workspaces/azure-devops/plugins/scaffolder-backend-module-azure-pipeline-actions/src/actions/example/example.test.ts
@@ -0,0 +1,34 @@
+import { PassThrough } from 'stream';
+import { createAcmeExampleAction } from './example';
+import { getVoidLogger } from '@backstage/backend-common';
+
+describe('acme:example', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should call action', async () => {
+    const action = createAcmeExampleAction();
+
+    const logger = getVoidLogger();
+    jest.spyOn(logger, 'info');
+
+    await action.handler({
+      input: {
+        myParameter: 'test',
+      },
+      workspacePath: '/tmp',
+      logger,
+      logStream: new PassThrough(),
+      output: jest.fn(),
+      createTemporaryDirectory() {
+        // Usage of createMockDirectory is recommended for testing of filesystem operations
+        throw new Error('Not implemented');
+      },
+    });
+
+    expect(logger.info).toHaveBeenCalledWith(
+      'Running example template with parameters: test',
+    );
+  });
+});
---
+++ workspaces/azure-devops/plugins/scaffolder-backend-module-azure-pipeline-actions/src/index.ts
@@ -0,0 +1,8 @@
+/***/
+/**
+ * The azure-pipeline-actions module for @backstage/plugin-scaffolder-backend.
+ *
+ * @packageDocumentation
+ */
+
+export * from './actions';
