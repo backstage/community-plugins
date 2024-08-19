3b45ff62e148f319f28a6317ef527b815475b1c1
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-08-19 11:47:12 +0100

    plugin(redhat-argocd): migrate of Janus argocd plugins to backstage/community-plugins repository (#856)
    * initial redhat-argocd workspace creation
    * add missing dependency react-router-dom
    * change skipLibCheck to true
    * remove --tsc form api reports
    * override api report
    * add missing devDependencies, react-dom and react
    * Run prettier
    * update names
    * update Changeset/cuddly-mice-jam.md file with a message
    * regenerate API reports
    * update pluginPackage name
    * update yarn.lock to match package.json
    * update links
    
    ---------
    
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>
    Signed-off-by: Bethany Griggs <bethanyngriggs@gmail.com>
    Co-authored-by: Beth Griggs <bethanyngriggs@gmail.com>

---
+++ workspaces/redhat-argocd/plugins/argocd/src/components/DeploymentSummary/DeploymentSummary.tsx
@@ -0,0 +1,189 @@
+import React from 'react';
+
+import { Table, TableColumn } from '@backstage/core-components';
+import { useEntity } from '@backstage/plugin-catalog-react';
+
+import { IconButton, Link } from '@material-ui/core';
+import ExternalLinkIcon from '@patternfly/react-icons/dist/esm/icons/external-link-alt-icon';
+import moment from 'moment';
+
+import { useApplications } from '../../hooks/useApplications';
+import { useArgocdConfig } from '../../hooks/useArgocdConfig';
+import { useArgocdViewPermission } from '../../hooks/useArgocdViewPermission';
+import { Application, HealthStatus, SyncStatuses } from '../../types';
+import {
+  getArgoCdAppConfig,
+  getCommitUrl,
+  getInstanceName,
+  isAppHelmChartType,
+} from '../../utils/utils';
+import AppSyncStatus from '../AppStatus/AppSyncStatus';
+import { AppHealthIcon } from '../AppStatus/StatusIcons';
+
+const DeploymentSummary = () => {
+  const { entity } = useEntity();
+
+  const { baseUrl, instances, intervalMs } = useArgocdConfig();
+  const instanceName = getInstanceName(entity) || instances?.[0]?.name;
+
+  const { appSelector, appName, projectName, appNamespace } =
+    getArgoCdAppConfig({ entity });
+
+  const { apps, loading, error } = useApplications({
+    instanceName,
+    intervalMs,
+    appSelector,
+    projectName,
+    appName,
+    appNamespace,
+  });
+
+  const hasArgocdViewAccess = useArgocdViewPermission();
+
+  const supportsMultipleArgoInstances = !!instances.length;
+  const getBaseUrl = (row: any): string | undefined => {
+    if (supportsMultipleArgoInstances && !baseUrl) {
+      return instances?.find(
+        value => value?.name === row.metadata?.instance?.name,
+      )?.url;
+    }
+    return baseUrl;
+  };
+
+  const columns: TableColumn<Application>[] = [
+    {
+      title: 'ArgoCD App',
+      field: 'name',
+      render: (row: Application): React.ReactNode =>
+        getBaseUrl(row) ? (
+          <Link
+            href={`${getBaseUrl(row)}/applications/${row?.metadata?.name}`}
+            target="_blank"
+            rel="noopener"
+          >
+            {row.metadata.name}{' '}
+            <IconButton color="primary" size="small">
+              <ExternalLinkIcon />
+            </IconButton>
+          </Link>
+        ) : (
+          row.metadata.name
+        ),
+    },
+    {
+      title: 'Namespace',
+      field: 'namespace',
+      render: (row: Application): React.ReactNode => {
+        return <>{row.spec.destination.namespace}</>;
+      },
+    },
+    {
+      title: 'Instance',
+      field: 'instance',
+      render: (row: Application): React.ReactNode => {
+        return <>{row.metadata?.instance?.name || instanceName}</>;
+      },
+    },
+    {
+      title: 'Server',
+      field: 'server',
+      render: (row: Application): React.ReactNode => {
+        return <>{row.spec.destination.server}</>;
+      },
+    },
+    {
+      title: 'Revision',
+      field: 'revision',
+      render: (row: Application): React.ReactNode => {
+        const historyList = row.status?.history ?? [];
+        const latestRev = historyList[historyList.length - 1];
+        const repoUrl = row?.spec?.source?.repoURL;
+        const commitUrl = isAppHelmChartType(row)
+          ? repoUrl
+          : getCommitUrl(
+              repoUrl,
+              latestRev?.revision,
+              entity?.metadata?.annotations || {},
+            );
+        return (
+          <Link href={commitUrl} target="_blank" rel="noopener">
+            {latestRev?.revision?.substring(0, 7) ?? '-'}
+          </Link>
+        );
+      },
+    },
+
+    {
+      id: 'test',
+      title: 'Last deployed',
+      field: 'lastdeployed',
+      customSort: (a: Application, b: Application) => {
+        const bHistory = b?.status?.history ?? [];
+        const bDeployedAt = bHistory?.[bHistory.length - 1]?.deployedAt;
+
+        const aHistory = a?.status?.history ?? [];
+        const aDeployedAt = aHistory?.[aHistory.length - 1]?.deployedAt;
+
+        return moment(aDeployedAt).diff(moment(bDeployedAt));
+      },
+      render: (row: Application): React.ReactNode => {
+        const history = row?.status?.history ?? [];
+        const finishedAt = history[history.length - 1]?.deployedAt;
+        return (
+          <>
+            {finishedAt
+              ? moment(finishedAt).local().format('D/MM/YYYY, H:mm:ss')
+              : null}
+          </>
+        );
+      },
+    },
+    {
+      title: 'Sync status',
+      field: 'syncstatus',
+      customSort: (a: Application, b: Application): number => {
+        const syncStatusOrder: string[] = Object.values(SyncStatuses);
+        return (
+          syncStatusOrder.indexOf(a?.status?.sync?.status) -
+          syncStatusOrder.indexOf(b?.status?.sync?.status)
+        );
+      },
+      render: (row: Application): React.ReactNode => (
+        <AppSyncStatus app={row} />
+      ),
+    },
+    {
+      title: 'Health status',
+      field: 'healthstatus',
+      customSort: (a: Application, b: Application): number => {
+        const healthStatusOrder: string[] = Object.values(HealthStatus);
+        return (
+          healthStatusOrder.indexOf(a?.status?.health?.status) -
+          healthStatusOrder.indexOf(b?.status?.health?.status)
+        );
+      },
+      render: (row: Application): React.ReactNode => (
+        <>
+          <AppHealthIcon status={row.status.health.status as HealthStatus} />{' '}
+          {row?.status?.health?.status}
+        </>
+      ),
+    },
+  ];
+
+  return !error && hasArgocdViewAccess ? (
+    <Table
+      title="Deployment summary"
+      options={{
+        paging: true,
+        search: false,
+        draggable: false,
+        padding: 'dense',
+      }}
+      isLoading={loading}
+      data={apps}
+      columns={columns}
+    />
+  ) : null;
+};
+export default DeploymentSummary;
---
+++ workspaces/redhat-argocd/plugins/argocd/src/components/DeploymentSummary/index.ts
@@ -0,0 +1,1 @@
+export { default as DeploymentSummary } from '../DeploymentSummary/DeploymentSummary';
