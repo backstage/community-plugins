d6b5a4f70bc6221305f430072da0a289b60b7802
Nurit Izrailov <nuriti@spotify.com>
2024-08-26 10:02:56 -0400

    fix yarn prettier
    Signed-off-by: Nurit Izrailov <nuriti@spotify.com>

--- workspaces/sonarqube/packages/app/src/App.tsx
+++ workspaces/sonarqube/packages/app/src/App.tsx
@@ -16,7 +16,11 @@ import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog, SignInPage } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
