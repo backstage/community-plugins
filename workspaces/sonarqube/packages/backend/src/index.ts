eac0d96198bc09f55e5b5d6401f9d4c60cae5940
Nurit Izrailov <nuriti@spotify.com>
2024-08-30 15:36:25 -0400

    add sonarqube example app
    Signed-off-by: Nurit Izrailov <nuriti@spotify.com>

---
+++ workspaces/sonarqube/packages/backend/src/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Hi!
+ *
+ * Note that this is an EXAMPLE Backstage backend. Please check the README.
+ *
+ * Happy hacking!
+ */
+
+import { createBackend } from '@backstage/backend-defaults';
+
+const backend = createBackend();
+
+backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+
+// auth plugins
+backend.add(import('@backstage/plugin-auth-backend'));
+// See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
+backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+// See https://backstage.io/docs/auth/guest/provider
+
+// catalog plugin
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+
+backend.add(import('@backstage-community/plugin-sonarqube-backend'));
+
+backend.start();
