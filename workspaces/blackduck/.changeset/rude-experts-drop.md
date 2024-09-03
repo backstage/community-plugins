322d40a7c45237a77c2df3dc7a9b03b888fdde50
VenkataKrishnaPriya <GurramVenkataKrishna.Priya@philips.com>
2024-09-03 14:26:29 +0530

    fixes missing configSchema
    Signed-off-by: VenkataKrishnaPriya <GurramVenkataKrishna.Priya@philips.com>

---
+++ workspaces/blackduck/.changeset/rude-experts-drop.md
@@ -0,0 +1,5 @@
+---
+'@backstage-community/plugin-blackduck-backend': patch
+---
+
+Fixed missing configSchema in blackduck-backend Plugin.
---
+++ workspaces/blackduck/plugins/blackduck-backend/config.d.ts
@@ -0,0 +1,12 @@
+export interface Config {
+    blackduck: {
+        /**
+           * @visibility frontend
+           */
+        host: string;
+        /**
+           * @visibility secret
+           */
+        token: string;
+    }
+}
