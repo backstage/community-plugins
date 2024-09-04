bc1ba0e4fb2c43ee741a8961c705c4a667050167
Alex McKay <amckay@spotify.com>
2024-09-04 15:51:33 -0400

    add example apps
    Signed-off-by: Alex McKay <amckay@spotify.com>

---
+++ workspaces/sentry/packages/app/src/apis.ts
@@ -0,0 +1,24 @@
+import {
+  ScmIntegrationsApi,
+  scmIntegrationsApiRef,
+  ScmAuth,
+} from '@backstage/integration-react';
+import {
+  AnyApiFactory,
+  configApiRef,
+  createApiFactory,
+} from '@backstage/core-plugin-api';
+import {
+  MockSentryApi,
+  sentryApiRef,
+} from '@backstage-community/plugin-sentry';
+
+export const apis: AnyApiFactory[] = [
+  createApiFactory({
+    api: scmIntegrationsApiRef,
+    deps: { configApi: configApiRef },
+    factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
+  }),
+  ScmAuth.createDefaultApiFactory(),
+  createApiFactory(sentryApiRef, new MockSentryApi()),
+];
