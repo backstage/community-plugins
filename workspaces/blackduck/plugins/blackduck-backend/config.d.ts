e8617d55a95b676783908b82f0270038814cc6a7
VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
2024-09-09 18:49:09 +0530

    fix: fix missing configSchema in blackduck-backend Plugin (#1150)
    * fixes missing configSchema
    
    Signed-off-by: VenkataKrishnaPriya <GurramVenkataKrishna.Priya@philips.com>
    
    * format config.d.ts
    
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    
    * format config.d.ts
    
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    
    * Update workspaces/blackduck/plugins/blackduck-backend/config.d.ts
    
    Co-authored-by: Kashish Mittal <113269381+04kash@users.noreply.github.com>
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    
    * format config.d.ts
    
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    
    * Update workspaces/blackduck/plugins/blackduck-backend/config.d.ts
    
    Co-authored-by: Andre Wanlin <67169551+awanlin@users.noreply.github.com>
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: VenkataKrishnaPriya <GurramVenkataKrishna.Priya@philips.com>
    Signed-off-by: VenkataKrishnaPriya <144668222+VenkataKrishnaPriya@users.noreply.github.com>
    Co-authored-by: Kashish Mittal <113269381+04kash@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <67169551+awanlin@users.noreply.github.com>

---
+++ workspaces/blackduck/plugins/blackduck-backend/config.d.ts
@@ -0,0 +1,29 @@
+export interface Config {
+  blackduck?: {
+    /**
+     * The default host name.
+     * @visibility frontend
+     */
+    default: string;
+    /**
+     * An array of Black Duck hosts.
+     */
+    hosts: Array<{
+      /**
+       * Host name identifier.
+       * @visibility frontend
+       */
+      name: string;
+      /**
+       * The host URL for the Black Duck instance.
+       * @visibility frontend
+       */
+      host: string;
+      /**
+       * API token for authentication.
+       * @visibility secret
+       */
+      token: string;
+    }>;
+  };
+}
