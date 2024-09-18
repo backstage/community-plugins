c41cb2a3de830aa3c97b66f334e9ca160de1e0b7
Kashish Mittal <kmittal@redhat.com>
2024-09-18 16:33:58 -0400

    update dev/index.ts
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- workspaces/3scale/plugins/3scale-backend/dev/index.ts
+++ workspaces/3scale/plugins/3scale-backend/dev/index.ts
@@ -3,7 +3,7 @@ import { catalogModule3ScaleEntityProvider } from '../src/module';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-catalog-backend'));
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
 backend.add(catalogModule3ScaleEntityProvider);
 
 backend.start();
