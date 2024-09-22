87cea9136787c7b16743ad90ce13563c95fec1d9
Vincenzo Scamporlino <vincenzos@spotify.com>
2024-09-22 22:50:30 +0200

    linkerd: fix linting issues
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- workspaces/linkerd/plugins/linkerd/src/components/OctopusGraph/MetricCounter.tsx
+++ workspaces/linkerd/plugins/linkerd/src/components/OctopusGraph/MetricCounter.tsx
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import React, { useState, useEffect } from 'react';
 import { Theme, makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -57,11 +73,11 @@ export const MetricCounter = ({
 
   return (
     <>
-      <span className={getClassName()}>{generateDiff()}</span>
-      <span>
+      <Typography className={getClassName()}>{generateDiff()}</Typography>
+      <Typography>
         {currentNumber}
         {suffix}
-      </span>
+      </Typography>
     </>
   );
 };
