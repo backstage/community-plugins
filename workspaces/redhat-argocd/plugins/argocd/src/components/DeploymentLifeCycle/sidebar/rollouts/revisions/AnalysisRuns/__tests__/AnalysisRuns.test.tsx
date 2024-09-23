800da78e24ff8749ad4f305bb3c80da73e2dfd6a
Vincenzo Scamporlino <vincenzos@spotify.com>
2024-09-23 14:07:26 +0200

    redhat-argocd: fix linting issues
    Signed-off-by: Vincenzo Scamporlino <vincenzos@spotify.com>

--- workspaces/redhat-argocd/plugins/argocd/src/components/DeploymentLifeCycle/sidebar/rollouts/revisions/AnalysisRuns/__tests__/AnalysisRuns.test.tsx
+++ workspaces/redhat-argocd/plugins/argocd/src/components/DeploymentLifeCycle/sidebar/rollouts/revisions/AnalysisRuns/__tests__/AnalysisRuns.test.tsx
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
@@ -51,7 +66,7 @@ describe('AnalysisRuns Component', () => {
       expect(chipElement).toBeInTheDocument();
 
       const iconElement = screen.getByTestId(
-        `analysisrun-${analysisRunStatus?.toLowerCase()}-icon`,
+        `analysisrun-${analysisRunStatus?.toLocaleLowerCase('en-US')}-icon`,
       );
       expect(iconElement).toBeInTheDocument();
     });
