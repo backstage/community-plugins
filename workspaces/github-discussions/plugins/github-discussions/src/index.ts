2cceff6598344a74bd808efa2a058a84d01a43eb
Min Kim <minkimcello@gmail.com>
2024-10-15 22:11:28 -0400

    Remove unnecessary files from default template

--- workspaces/github-discussions/plugins/github-discussions/src/index.ts
+++ workspaces/github-discussions/plugins/github-discussions/src/index.ts
@@ -13,4 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { githubDiscussionsPlugin, GithubDiscussionsPage } from './plugin';
+
+export { GithubDiscussionsSearchResultListItem } from './plugin';
