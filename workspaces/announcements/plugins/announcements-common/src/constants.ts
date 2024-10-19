5fd18dd12ddf44abb0d39ed22be3a67faa99807d
Kurt King <kurtaking@gmail.com>
2024-10-18 22:06:00 -0600

    migrate announcements-common plugin code
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- workspaces/announcements/plugins/announcements-common/src/constants.ts
+++ workspaces/announcements/plugins/announcements-common/src/constants.ts
@@ -13,4 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export {};
+export const EVENTS_TOPIC_ANNOUNCEMENTS = 'announcements';
+
+export const EVENTS_ACTION_CREATE_ANNOUNCEMENT = 'create_announcement';
+export const EVENTS_ACTION_UPDATE_ANNOUNCEMENT = 'update_announcement';
+export const EVENTS_ACTION_DELETE_ANNOUNCEMENT = 'delete_announcement';
+
+export const EVENTS_ACTION_CREATE_CATEGORY = 'create_category';
+export const EVENTS_ACTION_DELETE_CATEGORY = 'delete_category';
+
+export const SIGNALS_CHANNEL_ANNOUNCEMENTS = 'announcements:new';
--- workspaces/announcements/plugins/announcements-common/src/types.ts
+++ workspaces/announcements/plugins/announcements-common/src/types.ts
@@ -13,6 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './permissions';
-export * from './types';
-export * from './constants';
+export type Category = {
+  slug: string;
+  title: string;
+};
+
+export type Announcement = {
+  id: string;
+  category?: Category;
+  publisher: string;
+  title: string;
+  excerpt: string;
+  body: string;
+  created_at: string;
+  active: boolean;
+};
+
+export type AnnouncementsList = {
+  count: number;
+  results: Announcement[];
+};
+
+export type AnnouncementsFilters = {
+  max?: number;
+  offset?: number;
+  category?: string;
+  page?: number;
+  active?: boolean;
+};
+
+export type AnnouncementSignal = {
+  data: Announcement;
+};
