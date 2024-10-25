0eafa09f85f32524eb9cbb7cca10cecd653ab4fd
Kashish Mittal <kmittal@redhat.com>
2024-10-25 11:13:54 -0400

    chore(version-bump): add option to generate backstage version updates as either minor or patch updates with default as minor
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- scripts/ci/generate-version-bump-changeset.js
+++ scripts/ci/generate-version-bump-changeset.js
@@ -18,7 +18,7 @@
 
 // This script assumes that it is being ran from the plugins workspace,
 // for example: `/workspaces/azure-devops` and would be called like this:
-// `node ../../scripts/ci/generate-version-bump-changeset.js 1.29.1`
+// `node ../../scripts/ci/generate-version-bump-changeset.js 1.29.1 minor`
 
 import fs from 'fs-extra';
 import { getPackages } from '@manypkg/get-packages';
@@ -26,9 +26,11 @@ import { join } from 'path';
 
 async function main() {
   // Get the releaseVersion
-  const [script, releaseVersion] = process.argv.slice(1);
+  const [script, releaseVersion, versionBumpType] = process.argv.slice(1);
   if (!releaseVersion) {
-    throw new Error(`Argument must be ${script} <release-version>`);
+    throw new Error(
+      `Argument must be ${script} <release-version> <version-bump-type>`,
+    );
   }
 
   const workspacePlugins = join(process.cwd(), 'plugins');
@@ -46,7 +48,7 @@ async function main() {
   const { packages } = await getPackages(workspacePlugins);
   const packageEntries = packages
     .filter(p => p.packageJson.name.includes('@backstage-community'))
-    .map(p => `'${p.packageJson.name}': patch`);
+    .map(p => `'${p.packageJson.name}': ${versionBumpType}`);
 
   // Populate the changeset contents
   const changeset = `---
