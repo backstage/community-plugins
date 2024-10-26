2a6d6dd253e077abbf702a842179416ed1c1d627
Marcello Avila <avila.m.6@pg.com>
2024-10-25 22:59:17 -0600

    fix reports and config.d.ts
    Signed-off-by: Marcello Avila <avila.m.6@pg.com>

--- workspaces/shorturl/plugins/shorturl-backend/config.d.ts
+++ workspaces/shorturl/plugins/shorturl-backend/config.d.ts
@@ -1,14 +1,14 @@
 export interface Config {
-  shorturl: {
+  shorturl?: {
     /**
      * Lenght of the short URL
      * defaults to 8
      */
-    length: number;
+    length?: number;
     /**
      * Set of characters to use in the short URL
      * defaults to 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz0123456789-'
      */
-    alphabet: string;
+    alphabet?: string;
   };
 }
