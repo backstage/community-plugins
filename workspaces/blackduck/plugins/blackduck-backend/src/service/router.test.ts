09410a5abc85e56c5574acac323c899ce37bc0b4
Deepankumar Loganathan <deepan0433@gmail.com>
2024-10-26 11:19:43 +0200

    added blackduck-node library
    Signed-off-by: Deepankumar Loganathan <deepan0433@gmail.com>

--- workspaces/blackduck/plugins/blackduck-backend/src/service/router.test.ts
+++ workspaces/blackduck/plugins/blackduck-backend/src/service/router.test.ts
@@ -16,7 +16,7 @@
 import { mockServices } from '@backstage/backend-test-utils';
 import express from 'express';
 import request from 'supertest';
-import { BlackDuckConfig } from './BlackDuckConfig';
+import { BlackDuckConfig } from '@backstage-community/plugin-blackduck-node';
 
 import { createRouter } from './router';
 import { ConfigReader } from '@backstage/config';
