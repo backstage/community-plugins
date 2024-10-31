7a59fbb7afa5bf3d3ff02a0b510ccbf9dedab5df
Phillip Morton <Phiph@users.noreply.github.com>
2024-10-31 19:57:26 +0000

    Add azure:pipeline:run scaffolder action (#1358)
    * init pipeline scaffolder
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    
    * add template for the pipeline api with v14 of node-api
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add template for the pipeline api with       webApi();
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add working scaffolder action for run.
    
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * Commit Run Pipeline example
    
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * update lock file
    
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add logging, and refactor.
    
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * rename action file
    
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add change set
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * remove @bacskatge/backend-common, and unused code.
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * rename azure scaffolder action package
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add new line
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * delete commented code, and correct host on schema properties.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * rename, and move devopsRunPipeline action.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add examples
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add yaml
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * Update workspaces/azure-devops/plugins/scaffolder-backend-module-azure-devops/src/module.ts
    
    Co-authored-by: Andre Wanlin <67169551+awanlin@users.noreply.github.com>
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    
    * Add documentation
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * add examples into the schema.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * remove quotes on readme links
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * update changeset to patch
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * remove private flag for package.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * apply yarn fix to devops scaffolder module
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * apply yarn:prettier to devops scaffolder module.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    * apply yarn backstage-cli repo fix --publish to scaffolder package.
    
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Phillip Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <Phiph@users.noreply.github.com>
    Signed-off-by: Phill Morton <phill.morton@theaccessgroup.com>
    Co-authored-by: Andre Wanlin <67169551+awanlin@users.noreply.github.com>

---
+++ workspaces/azure-devops/plugins/scaffolder-backend-module-azure-devops/src/actions/devopsRunPipeline.ts
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import {
+  DefaultAzureDevOpsCredentialsProvider,
+  ScmIntegrationRegistry,
+} from '@backstage/integration';
+import { examples } from './devopsRunPipeline.examples';
+
+import { InputError } from '@backstage/errors';
+import {
+  getBearerHandler,
+  getPersonalAccessTokenHandler,
+  WebApi,
+} from 'azure-devops-node-api';
+import {
+  RunPipelineParameters,
+  RunState,
+} from 'azure-devops-node-api/interfaces/PipelinesInterfaces';
+/**
+ * Creates an `acme:example` Scaffolder action.
+ *
+ * @remarks
+ *
+ * See {@link https://example.com} for more information.
+ *
+ * @public
+ */
+export function createAzureDevopsRunPipelineAction(options: {
+  integrations: ScmIntegrationRegistry;
+}) {
+  const { integrations } = options;
+
+  return createTemplateAction<{
+    host?: string;
+    organization: string;
+    pipelineId: string;
+    project: string;
+    branch?: string;
+    token?: string;
+  }>({
+    id: 'azure:pipeline:run',
+    examples,
+    schema: {
+      input: {
+        required: ['organization', 'pipelineId', 'project'],
+        type: 'object',
+        properties: {
+          host: {
+            type: 'string',
+            title: 'Host',
+            description: 'The host of Azure DevOps. Defaults to dev.azure.com',
+          },
+          organization: {
+            type: 'string',
+            title: 'Organization',
+            description: 'The name of the Azure DevOps organization.',
+          },
+          pipelineId: {
+            type: 'string',
+            title: 'Pipeline ID',
+            description: 'The pipeline ID.',
+          },
+          project: {
+            type: 'string',
+            title: 'Project',
+            description: 'The name of the Azure project.',
+          },
+          branch: {
+            title: 'Repository Branch',
+            type: 'string',
+            description: "The branch of the pipeline's repository.",
+          },
+        },
+      },
+      output: {
+        type: 'object',
+        required: ['pipelineRunUrl'],
+        properties: {
+          pipelineRunUrl: {
+            type: 'string',
+          },
+          pipelineRunStatus: {
+            type: 'string',
+          },
+        },
+      },
+    },
+    async handler(ctx) {
+      const {
+        host = 'dev.azure.com',
+        organization,
+        pipelineId,
+        project,
+        branch,
+      } = ctx.input;
+
+      const url = `https://${host}/${organization}`;
+      const credentialProvider =
+        DefaultAzureDevOpsCredentialsProvider.fromIntegrations(integrations);
+      const credentials = await credentialProvider.getCredentials({ url: url });
+
+      if (credentials === undefined && ctx.input.token === undefined) {
+        throw new InputError(
+          `No credentials provided ${url}, please check your integrations config`,
+        );
+      }
+
+      const authHandler =
+        ctx.input.token || credentials?.type === 'pat'
+          ? getPersonalAccessTokenHandler(ctx.input.token ?? credentials!.token)
+          : getBearerHandler(credentials!.token);
+
+      const webApi = new WebApi(url, authHandler);
+      const client = await webApi.getPipelinesApi();
+      const createOptions: RunPipelineParameters = {
+        resources: {
+          repositories: {
+            self: {
+              refName: branch ? `refs/heads/${branch}` : `refs/heads/main`,
+            },
+          },
+        },
+      };
+
+      // Log the createOptions object in a readable format
+      ctx.logger.debug(
+        'Create options for running the pipeline:',
+        JSON.stringify(createOptions, null, 2),
+      );
+
+      const pipelineRun = await client.runPipeline(
+        createOptions,
+        project,
+        parseInt(pipelineId, 10),
+      );
+
+      // Log the pipeline run URL
+      ctx.logger.info(`Pipeline run URL: ${pipelineRun._links.web.href}`);
+
+      // Log the pipeline run state if available
+      if (pipelineRun.state) {
+        ctx.logger.info(`Pipeline run state: ${RunState[pipelineRun.state]}`);
+      }
+
+      // Log the pipeline run result if available
+      if (pipelineRun.result) {
+        ctx.logger.info(
+          `Pipeline run result: ${pipelineRun.result.toString()}`,
+        );
+      }
+
+      // Log the entire pipeline run object for debugging purposes
+      ctx.logger.debug(
+        `Pipeline run details: ${JSON.stringify(pipelineRun, null, 2)}`,
+      );
+
+      ctx.output('pipelineRunUrl', pipelineRun._links.web.href);
+      ctx.output('pipelineRunStatus', pipelineRun.result?.toString());
+    },
+  });
+}
