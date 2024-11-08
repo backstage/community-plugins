3aa6f391c5d886222cc4fc12d96689bcb1fc540a
Oleksandr Andriienko <oandriie@redhat.com>
2024-11-08 17:07:33 +0200

    feat(rbac): add rbac-backend plugin to app
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- workspaces/rbac/packages/backend/src/index.ts
+++ workspaces/rbac/packages/backend/src/index.ts
@@ -39,11 +39,7 @@ backend.add(
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 
 // permission plugin
-backend.add(import('@backstage/plugin-permission-backend/alpha'));
-// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
-backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-);
+backend.add(import('@backstage-community/plugin-rbac-backend'));
 
 // search plugin
 backend.add(import('@backstage/plugin-search-backend/alpha'));
