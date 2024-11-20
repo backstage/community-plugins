5f5276299f4ec829da680aaf35efc28064fc4d8b
Andre Wanlin <awanlin@spotify.com>
2024-11-20 13:39:16 -0600

    blackduck - Deprecated legacy backend
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/blackduck/plugins/blackduck-backend/src/service/router.ts
+++ workspaces/blackduck/plugins/blackduck-backend/src/service/router.ts
@@ -35,7 +35,10 @@ import {
 import { BlackDuckRestApi } from '../api/BlackDuckRestApi';
 import { BlackDuckConfig } from './BlackDuckConfig';
 
-/** @public */
+/**
+ * @deprecated Please migrate to the new backend system as this will be removed in the future.
+ *
+ * @public */
 export interface RouterOptions {
   logger: LoggerService;
   config: Config;
@@ -45,7 +48,10 @@ export interface RouterOptions {
   blackDuckConfig: BlackDuckConfig;
 }
 
-/** @public */
+/**
+ * @deprecated Please migrate to the new backend system as this will be removed in the future.
+ *
+ * @public */
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
