0a0e2047742a122af7f5b50916ce112d48d24810
Christophe Augello <caugello@redhat.com>
2024-12-02 08:26:44 +0100

    feat(plugin): multi-source-security-viewer
    Signed-off-by: Christophe Augello <caugello@redhat.com>

---
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/components/PipelineRunList/PipelineRunTablePagination.tsx
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import {
+  Box,
+  IconButton,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import {
+  FirstPage,
+  LastPage,
+  NavigateBefore,
+  NavigateNext,
+} from '@mui/icons-material';
+
+type PipelineRunTablePaginationProps = {
+  count: number;
+  page: number;
+  rowSize: number[];
+  rowsPerPage: number;
+  handleChangePage: (newPage: number) => void;
+  handleChangeRowsPerPage: (rows: number) => void;
+};
+
+const useStyles = makeStyles(() => ({
+  actionsBox: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  typography: {
+    fontSize: '1rem',
+  },
+}));
+
+export const PipelineRunTablePagination: React.FC<
+  PipelineRunTablePaginationProps
+> = ({
+  count,
+  page,
+  rowSize,
+  rowsPerPage,
+  handleChangePage,
+  handleChangeRowsPerPage,
+}) => {
+  const classes = useStyles();
+  const [pageInput, setPageInput] = React.useState('1');
+
+  const handleRowsChange = (event: React.ChangeEvent<{ value: any }>) => {
+    handleChangeRowsPerPage(parseInt(event.target.value, 10));
+  };
+
+  const totalPages = React.useMemo(
+    () => Math.ceil(count / rowsPerPage),
+    [count, rowsPerPage],
+  );
+
+  const previousPage = React.useMemo(() => page - 1, [page]);
+  const nextPage = React.useMemo(() => page + 1, [page]);
+  const firstPage = React.useMemo(() => 0, []);
+  const lastPage = React.useMemo(() => totalPages - 1, [totalPages]);
+  const isFirstPage = React.useMemo(() => page === 0, [page]);
+  const isLastPage = React.useMemo(
+    () => page === totalPages - 1,
+    [totalPages, page],
+  );
+
+  const selectItemText = React.useMemo(() => {
+    // display for ex: 1 - 10 of 100
+    return `${page === 0 ? 1 : page * rowsPerPage + 1} - ${
+      rowsPerPage * (page + 1) > count ? count : rowsPerPage * (page + 1)
+    } of ${count}`;
+  }, [count, page, rowsPerPage]);
+
+  const handleInputChange = (value: string) => {
+    const newPage = parseInt(value, 10);
+    if (newPage > 0 && newPage <= totalPages) {
+      handleChangePage(newPage - 1); // -1 as page is 0-indexed
+    }
+    setPageInput(isNaN(newPage) ? '' : newPage.toString());
+  };
+
+  React.useEffect(() => {
+    setPageInput((page + 1).toString());
+  }, [page]);
+
+  return (
+    <React.Fragment>
+      <Box className={classes.actionsBox} data-testid="pipeline-run-pagination">
+        <Select
+          variant="standard"
+          renderValue={() => selectItemText}
+          value={rowsPerPage}
+          aria-label="rows per page"
+          data-testid="rows-per-page"
+          autoWidth
+          disableUnderline
+          displayEmpty
+          onChange={handleRowsChange}
+          inputProps={{
+            style: { minWidth: 0, padding: 0 },
+          }}
+        >
+          {rowSize.map(size => (
+            <MenuItem key={size} value={size}>
+              {size} per page
+            </MenuItem>
+          ))}
+        </Select>
+
+        <IconButton
+          onClick={() => handleChangePage(firstPage)}
+          disabled={isFirstPage}
+          aria-label="first page"
+          data-testid="first-page"
+          size="small"
+        >
+          <FirstPage />
+        </IconButton>
+        <IconButton
+          onClick={() => handleChangePage(previousPage)}
+          disabled={isFirstPage}
+          aria-label="previous page"
+          data-testid="previous-page"
+          size="small"
+        >
+          <NavigateBefore />
+        </IconButton>
+        <TextField
+          size="small"
+          value={pageInput}
+          variant="outlined"
+          aria-label="page number"
+          aria-describedby="page-number"
+          id="page-number-input"
+          data-testid="page-number"
+          style={{ marginRight: '0.5rem' }}
+          onChange={e => handleInputChange(e.target.value)}
+          inputProps={{
+            style: { textAlign: 'center', width: '2rem' },
+          }}
+        />
+        <Typography variant="body2" style={{ fontSize: '1rem' }}>
+          of {totalPages}
+        </Typography>
+        <IconButton
+          onClick={() => handleChangePage(nextPage)}
+          disabled={isLastPage}
+          aria-label="next page"
+          data-testid="next-page"
+          size="small"
+        >
+          <NavigateNext />
+        </IconButton>
+        <IconButton
+          onClick={() => handleChangePage(lastPage)}
+          disabled={isLastPage}
+          aria-label="last page"
+          data-testid="last-page"
+          size="small"
+        >
+          <LastPage />
+        </IconButton>
+      </Box>
+    </React.Fragment>
+  );
+};
---
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/components/PipelineRunList/PipelineRunTableRowActions.tsx
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import { Box, IconButton, makeStyles, Theme } from '@material-ui/core';
+import Tooltip from '@mui/material/Tooltip';
+import PipelineRunOutput from './PipelineRunOutput';
+import { PipelineRunLogs } from '../PipelineRunLogs/PipelineRunLogs';
+import { DialogLauncher } from '../DialogLauncher/DialogLauncher';
+import { PipelineRunResult } from '../../models/pipelineRunResult';
+import { ViewLogsIcon } from '../Icons/LogsIcon';
+import { OutputIcon } from '../Icons/OutputIcon';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  boxActions: {
+    display: 'flex',
+    alignItems: 'left',
+    gap: '0.5rem',
+  },
+  icon: {
+    padding: 0,
+    '&:nth-child(even)': {
+      marginLeft: theme.spacing(1),
+    },
+  },
+}));
+
+type PipelineRunTableRowActionsProps = {
+  pr: PipelineRunResult;
+};
+
+export const PipelineRunTableRowActions: React.FC<
+  PipelineRunTableRowActionsProps
+> = ({ pr }) => {
+  const [openOutput, setOpenOutput] = React.useState(false);
+  const [openLogs, setOpenLogs] = React.useState(false);
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <DialogLauncher
+        key={`${pr.id}-output`}
+        title={pr.id}
+        open={openOutput}
+        onClose={() => setOpenOutput(false)}
+        component={PipelineRunOutput}
+        componentProps={{ pr: pr }}
+        fullWidth
+        maxWidth="xl"
+      />
+
+      <DialogLauncher
+        key={`${pr.id}-logs`}
+        title={pr.id}
+        open={openLogs}
+        onClose={() => setOpenLogs(false)}
+        component={PipelineRunLogs}
+        componentProps={{ pr: pr }}
+        fullWidth
+        maxWidth="xl"
+      />
+
+      <Box className={classes.boxActions}>
+        <Tooltip
+          title={
+            pr.hasSteps ? 'View Logs' : 'Logs are not available for this run'
+          }
+          arrow
+        >
+          <Box>
+            <IconButton
+              data-testid="button-logs"
+              className={classes.icon}
+              disabled={!pr.hasSteps}
+              onClick={() => setOpenLogs(true)}
+            >
+              <ViewLogsIcon disabled={!pr.hasSteps} />
+            </IconButton>
+          </Box>
+        </Tooltip>
+        <Tooltip
+          title={
+            pr.hasNoScanResults
+              ? 'Scan Results are not available for this run'
+              : 'View Scan Results'
+          }
+          arrow
+        >
+          <Box>
+            <IconButton
+              className={classes.icon}
+              data-testid="button-output"
+              disabled={pr.hasNoScanResults}
+              onClick={() => setOpenOutput(true)}
+            >
+              <OutputIcon disabled={pr.hasNoScanResults} />
+            </IconButton>
+          </Box>
+        </Tooltip>
+      </Box>
+    </React.Fragment>
+  );
+};
