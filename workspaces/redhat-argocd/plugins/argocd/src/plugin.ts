849416c4b3e508d2d4aaf2d847ebb89c1a43b72c
Bryan Ramos <bramos@redhat.com>
2024-12-06 09:17:07 -0500

    plugin(redhat-argocd): upgrade to patternfly v6
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- workspaces/redhat-argocd/plugins/argocd/src/plugin.ts
+++ workspaces/redhat-argocd/plugins/argocd/src/plugin.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 import '@patternfly/react-core/dist/styles/base.css';
-import '@patternfly/patternfly/patternfly-theme-dark.css';
 
 import {
   configApiRef,
