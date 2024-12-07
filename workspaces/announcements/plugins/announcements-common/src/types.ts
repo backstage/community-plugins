c53bdfe8c8dba3d6d14ec5ca5d6d27643eeaa253
Kurt King <kurtaking@gmail.com>
2024-12-06 20:26:28 -0700

    migrate announcements-common
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ workspaces/announcements/plugins/announcements-common/src/types.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export type Category = {
+  slug: string;
+  title: string;
+};
+
+export type Announcement = {
+  id: string;
+  category?: Category;
+  publisher: string;
+  title: string;
+  excerpt: string;
+  body: string;
+  created_at: string;
+  active: boolean;
+};
+
+export type AnnouncementsList = {
+  count: number;
+  results: Announcement[];
+};
+
+export type AnnouncementsFilters = {
+  max?: number;
+  offset?: number;
+  category?: string;
+  page?: number;
+  active?: boolean;
+};
+
+export type AnnouncementSignal = {
+  data: Announcement;
+};
