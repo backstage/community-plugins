5d6e5c55f3f8fa5c845c7d6da312d83028108d11
Kurt King <kurtaking@gmail.com>
2024-12-25 09:57:06 -0700

    migrate announcements-react
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ workspaces/announcements/plugins/announcements-react/src/apis/AnnouncementsApi.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { DateTime } from 'luxon';
+import { createApiRef } from '@backstage/core-plugin-api';
+import { CreateAnnouncementRequest, CreateCategoryRequest } from './types';
+import {
+  Announcement,
+  AnnouncementsList,
+  Category,
+} from '@procore-oss/backstage-plugin-announcements-common';
+
+export const announcementsApiRef = createApiRef<AnnouncementsApi>({
+  id: 'plugin.announcements.service',
+});
+
+export interface AnnouncementsApi {
+  announcements(opts: {
+    max?: number;
+    page?: number;
+    category?: string;
+    active?: boolean;
+  }): Promise<AnnouncementsList>;
+  announcementByID(id: string): Promise<Announcement>;
+
+  createAnnouncement(request: CreateAnnouncementRequest): Promise<Announcement>;
+  updateAnnouncement(
+    id: string,
+    request: CreateAnnouncementRequest,
+  ): Promise<Announcement>;
+  deleteAnnouncementByID(id: string): Promise<void>;
+
+  categories(): Promise<Category[]>;
+  createCategory(request: CreateCategoryRequest): Promise<void>;
+  deleteCategory(slug: string): Promise<void>;
+
+  lastSeenDate(): DateTime;
+  markLastSeenDate(date: DateTime): void;
+}
---
+++ workspaces/announcements/plugins/announcements-react/src/translation.ts
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { createTranslationRef } from '@backstage/core-plugin-api/alpha';
+
+/** @public */
+export const translationRef = createTranslationRef({
+  id: 'announcements',
+  messages: {
+    announcementForm: {
+      title: 'Title',
+      excerpt: 'Excerpt',
+      active: 'Active',
+      submit: 'Submit',
+      editAnnouncement: 'Edit announcement',
+      newAnnouncement: 'New announcement',
+      categoryInput: {
+        create: 'Create',
+        label: 'Category',
+      },
+    },
+    announcementsPage: {
+      newAnnouncement: 'New announcement',
+      genericNew: 'New',
+      card: {
+        by: 'By',
+        in: 'in',
+        edit: 'EDIT',
+        delete: 'DELETE',
+      },
+      grid: {
+        announcementDeleted: 'Announcement deleted.',
+      },
+      contextMenu: {
+        admin: 'Admin',
+        categories: 'Categories',
+      },
+    },
+    deleteDialog: {
+      title: 'Are you sure you want to delete this announcement?',
+      cancel: 'Cancel',
+      delete: 'Delete',
+    },
+    announcementsCard: {
+      seeAll: 'See all',
+      announcements: 'Announcements',
+      new: 'New',
+      in: 'in',
+      noAnnouncements: 'No announcements yet, want to',
+      addOne: 'add one',
+    },
+    announcementSearchResultListItem: {
+      published: 'Published',
+      announcement: 'Announcement',
+    },
+    announcementsTimeline: {
+      noAnnouncements: 'No announcements',
+      error: 'Error',
+    },
+    categoriesForm: {
+      newCategory: 'New category',
+      editCategory: 'Edit category',
+      titleLabel: 'Title',
+      submit: 'Submit',
+    },
+    categoriesTable: {
+      categoryDeleted: 'Category deleted.',
+      slug: 'Slug',
+      title: 'Title',
+      actions: 'Actions',
+      addTooltip: 'Add',
+      noCategoriesFound: 'No categories found.',
+    },
+    categoriesPage: {
+      title: 'Categories',
+      subtitle: 'Manage announcement categories',
+    },
+    createAnnouncementPage: {
+      alertMessage: 'Announcement created.',
+      alertMessageWithNewCategory: 'with new category',
+    },
+    editAnnouncementPage: {
+      updatedMessage: 'Announcement updated.',
+      notFoundMessage: 'Unable to find announcement',
+      edit: 'Edit',
+    },
+    newAnnouncementBanner: {
+      markAsSeen: 'Mark as seen',
+    },
+    newCategoryDialog: {
+      createdMessage: 'Category created.',
+      newCategory: 'New category',
+      title: 'Title',
+      cancelButton: 'Cancel',
+      createButton: 'Create',
+    },
+    admin: {
+      adminPortal: {
+        announcementsLabels: 'Announcements',
+        categoriesLabel: 'Categories',
+        title: 'Admin Portal for Announcements',
+        subtitle: 'Manage announcements and categories',
+      },
+      announecementsContent: {
+        alertMessage: 'Announcement created.',
+        alertMessageWithNewCategory: 'with new category',
+        cancelButton: 'Cancel',
+        createButton: 'Create Announcement',
+        announcements: 'Announcements',
+        noAnnouncementsFound: 'No announcements found',
+        table: {
+          title: 'Title',
+          body: 'Body',
+          publisher: 'Publisher',
+          category: 'Category',
+          status: 'Status',
+          actions: 'Actions',
+          active: 'Active',
+          inactive: 'Inactive',
+        },
+      },
+      categoriesContent: {
+        createdMessage: 'created',
+        deletedMessage: 'Category deleted.',
+        cancelButton: 'Cancel',
+        createButton: 'Create category',
+        table: {
+          categoryDeleted: 'Category deleted.',
+          slug: 'Slug',
+          title: 'Title',
+          actions: 'Actions',
+          addTooltip: 'Add',
+          noCategoriesFound: 'No categories found.',
+        },
+      },
+    },
+  },
+});
