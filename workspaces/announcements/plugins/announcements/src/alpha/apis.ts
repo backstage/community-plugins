8a888f5a2baeb0707ddc53571c439d5b032a01e1
Kurt King <kurtaking@gmail.com>
2024-12-25 09:05:19 -0700

    Migrate announcements frontend plugin from procore-oss (#2393)
    * First pass at migrating frontend
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * fix lock file
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * fix: yarn backstage-cli repo fix --publish
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * fix root package.json
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * fix react and react-use
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * add api report
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    ---------
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    Co-authored-by: Andre Wanlin <67169551+awanlin@users.noreply.github.com>

---
+++ workspaces/announcements/plugins/announcements/src/alpha/apis.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  ApiBlueprint,
+  createApiFactory,
+  discoveryApiRef,
+  errorApiRef,
+  fetchApiRef,
+  identityApiRef,
+} from '@backstage/frontend-plugin-api';
+import { announcementsApiRef } from '@procore-oss/backstage-plugin-announcements-react';
+import { AnnouncementsClient } from '../api';
+
+/**
+ * @alpha
+ */
+export const announcementsApiExtension = ApiBlueprint.make({
+  params: {
+    factory: createApiFactory({
+      api: announcementsApiRef,
+      deps: {
+        discoveryApi: discoveryApiRef,
+        identityApi: identityApiRef,
+        fetchApi: fetchApiRef,
+        errorApi: errorApiRef,
+      },
+      factory: ({ discoveryApi, identityApi, fetchApi, errorApi }) =>
+        new AnnouncementsClient({
+          discoveryApi,
+          identityApi,
+          fetchApi,
+          errorApi,
+        }),
+    }),
+  },
+});
---
+++ workspaces/announcements/plugins/announcements/src/components/Admin/AdminPortal/AdminPortal.tsx
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { useState } from 'react';
+import { Page, Header, Content } from '@backstage/core-components';
+import { AnnouncementsContent } from '../AnnouncementsContent';
+import { CategoriesContent } from '../CategoriesContent';
+import { RequirePermission } from '@backstage/plugin-permission-react';
+import { useAnnouncementsTranslation } from '@procore-oss/backstage-plugin-announcements-react';
+import { makeStyles, Tab } from '@material-ui/core';
+import { TabContext, TabList, TabPanel } from '@material-ui/lab';
+import { announcementCreatePermission } from '@backstage-community/plugin-announcements-common';
+
+const useStyles = makeStyles(() => ({
+  tabPanel: {
+    paddingLeft: '0px',
+    paddingRight: '0px',
+  },
+}));
+
+type AdminPortalProps = {
+  themeId?: string;
+  title?: string;
+  subtitle?: string;
+};
+
+const AdminPortalContent = () => {
+  const classes = useStyles();
+  const [tab, setTab] = useState('announcements');
+  const { t } = useAnnouncementsTranslation();
+  const handleChange = (_event: React.ChangeEvent<{}>, tabValue: string) => {
+    setTab(tabValue);
+  };
+
+  return (
+    <TabContext value={tab}>
+      <TabList onChange={handleChange}>
+        <Tab
+          label={t('admin.adminPortal.announcementsLabels')}
+          value="announcements"
+        />
+        <Tab
+          label={t('admin.adminPortal.categoriesLabel')}
+          value="categories"
+        />
+      </TabList>
+      <TabPanel value="announcements" className={classes.tabPanel}>
+        <AnnouncementsContent />
+      </TabPanel>
+      <TabPanel value="categories" className={classes.tabPanel}>
+        <CategoriesContent />
+      </TabPanel>
+    </TabContext>
+  );
+};
+
+/** @public */
+export const AdminPortal = (props?: AdminPortalProps) => {
+  const { title, subtitle, themeId } = props ?? {};
+  const { t } = useAnnouncementsTranslation();
+
+  return (
+    <Page themeId={themeId ?? 'tool'}>
+      <Header
+        title={title ?? t('admin.adminPortal.title')}
+        subtitle={subtitle ?? t('admin.adminPortal.title')}
+      />
+      <RequirePermission permission={announcementCreatePermission}>
+        <Content>
+          <AdminPortalContent />
+        </Content>
+      </RequirePermission>
+    </Page>
+  );
+};
