70cf7c999a89705a1f7e18c25ad4330ae73f662d
Maica Ballangan <hballangan@mdsol.com>
2025-01-17 20:07:45 +0900

    fix imported dependency
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>

--- workspaces/tech-insights/plugins/tech-insights-maturity/src/components/MaturityScorePage/maturityTableRows.tsx
+++ workspaces/tech-insights/plugins/tech-insights-maturity/src/components/MaturityScorePage/maturityTableRows.tsx
@@ -38,7 +38,7 @@ import Typography from '@mui/material/Typography';
 import { makeStyles, styled } from '@mui/styles';
 import React from 'react';
 import { MaturityRankAvatar } from '../MaturityRankAvatar';
-import { InsightFacts } from '@backstage-community/plugin-tech-insights-common';
+import { InsightFacts } from '@backstage-community/plugin-tech-insights-common/client';
 
 interface Props {
   checks: MaturityCheckResult[];
