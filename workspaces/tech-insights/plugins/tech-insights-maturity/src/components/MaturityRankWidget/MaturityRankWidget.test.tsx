42bc285641b4dc0a3d304af7892f90088e4106ef
Maica Ballangan <hballangan@mdsol.com>
2025-01-27 17:46:39 +0900

    New tech insights maturity plugin based on tech-insights  (#2464)
    * init tech-insights-maturity
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * add eslint
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * rename plugin export naming
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update readme
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * MaturityApiRef to extend TechInsightsApiRef
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * sanitize and refactor
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * export EntityMaturitySummaryContent
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * fix remaining issue
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update package dependencies
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * resolve use-react tsc issue
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * run prettier
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * generate build report
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * use workspace package
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * yarn fix
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * move tech-insights-maturity to  tech-insights workspace
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * add missing dependency
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * fix imported dependency
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor getMaturityScore
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove custom org implementations
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor MaturityLink
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove non-common plugin dependencies in maturity-common
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove unused components
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove unused components
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * Update workspaces/tech-insights/plugins/tech-insights-maturity/src/api/MaturityApi.ts
    
    Co-authored-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update packages and report
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update yarn packages
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * sync merge conflict
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * run yarn dedupe
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * generate api report md
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor maturityrank
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    ---------
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    Co-authored-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Harmen Brinkman <harmen.brinkman@trimm.nl>

---
+++ workspaces/tech-insights/plugins/tech-insights-maturity/src/components/MaturityRankWidget/MaturityRankWidget.test.tsx
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  EntityProvider,
+  entityRouteRef,
+} from '@backstage/plugin-catalog-react';
+import { renderInTestApp, TestApiProvider } from '@backstage/test-utils';
+import {
+  MaturityRank,
+  Rank,
+} from '@backstage-community/plugin-tech-insights-maturity-common';
+import React from 'react';
+import { MaturityRankWidget } from './MaturityRankWidget';
+import { MaturityApi, maturityApiRef } from '../../api';
+
+const entity = {
+  apiVersion: 'backstage.io/v1alpha1',
+  kind: 'Component',
+  metadata: {
+    namespace: 'default',
+    name: 'bingaux-sources',
+    title: 'Bingaux Sources',
+    stakeholders: [
+      {
+        role: 'architect',
+        email: 'some@email.com',
+      },
+    ],
+    tags: ['python', 'csharp'],
+  },
+  spec: {
+    type: 'service',
+    lifecycle: 'production',
+    owner: 'team-119',
+    system: 'api-tools',
+  },
+};
+
+describe('<MaturityRankWidget />', () => {
+  afterEach(() => jest.resetAllMocks());
+  it('shows maturity rank widget with bronze rank', async () => {
+    const rank: MaturityRank = {
+      rank: Rank.Bronze,
+      isMaxRank: true,
+    };
+
+    const scoringApi: Partial<MaturityApi> = {
+      getMaturityRank: jest.fn().mockResolvedValue(rank),
+    };
+
+    const { getByAltText } = await renderInTestApp(
+      <TestApiProvider apis={[[maturityApiRef, scoringApi]]}>
+        <EntityProvider entity={entity}>
+          <MaturityRankWidget entity={entity} />
+        </EntityProvider>
+      </TestApiProvider>,
+      {
+        mountedRoutes: {
+          '/catalog/:namespace/:kind/:name': entityRouteRef,
+        },
+      },
+    );
+
+    expect(getByAltText(/Bronze/)).toBeInTheDocument(); // image
+  });
+
+  it('shows maturity rank widget with Stone rank', async () => {
+    const rank: MaturityRank = {
+      rank: Rank.Stone,
+      isMaxRank: false,
+    };
+
+    const scoringApi: Partial<MaturityApi> = {
+      getMaturityRank: jest.fn().mockResolvedValue(rank),
+    };
+
+    const { getByAltText } = await renderInTestApp(
+      <TestApiProvider apis={[[maturityApiRef, scoringApi]]}>
+        <EntityProvider entity={entity}>
+          <MaturityRankWidget entity={entity} />
+        </EntityProvider>
+      </TestApiProvider>,
+      {
+        mountedRoutes: {
+          '/catalog/:namespace/:kind/:name': entityRouteRef,
+        },
+      },
+    );
+
+    expect(getByAltText(/Stone/)).toBeInTheDocument(); // image
+  });
+
+  it('shows maturity rank widget with silver rank', async () => {
+    const rank: MaturityRank = {
+      rank: Rank.Silver,
+      isMaxRank: true,
+    };
+
+    const scoringApi: Partial<MaturityApi> = {
+      getMaturityRank: jest.fn().mockResolvedValue(rank),
+    };
+
+    const { getByAltText } = await renderInTestApp(
+      <TestApiProvider apis={[[maturityApiRef, scoringApi]]}>
+        <EntityProvider entity={entity}>
+          <MaturityRankWidget entity={entity} />
+        </EntityProvider>
+      </TestApiProvider>,
+      {
+        mountedRoutes: {
+          '/catalog/:namespace/:kind/:name': entityRouteRef,
+        },
+      },
+    );
+
+    expect(getByAltText(/Silver/)).toBeInTheDocument(); // image
+  });
+
+  it('shows maturity rank widget with gold rank', async () => {
+    const rank: MaturityRank = {
+      rank: Rank.Gold,
+      isMaxRank: true,
+    };
+
+    const scoringApi: Partial<MaturityApi> = {
+      getMaturityRank: jest.fn().mockResolvedValue(rank),
+    };
+
+    const { getByAltText } = await renderInTestApp(
+      <TestApiProvider apis={[[maturityApiRef, scoringApi]]}>
+        <EntityProvider entity={entity}>
+          <MaturityRankWidget entity={entity} />
+        </EntityProvider>
+      </TestApiProvider>,
+      {
+        mountedRoutes: {
+          '/catalog/:namespace/:kind/:name': entityRouteRef,
+        },
+      },
+    );
+
+    expect(getByAltText(/Gold/)).toBeInTheDocument(); // image
+  });
+});
---
+++ workspaces/tech-insights/plugins/tech-insights-maturity/src/components/MaturitySummaryTable/MaturitySummaryTable.test.tsx
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** @jest-environment jsdom */
+import {
+  Entity,
+  RELATION_HAS_PART,
+  RELATION_PART_OF,
+} from '@backstage/catalog-model';
+import {
+  CatalogApi,
+  catalogApiRef,
+  EntityProvider,
+  entityRouteRef,
+} from '@backstage/plugin-catalog-react';
+import { renderInTestApp, TestApiProvider } from '@backstage/test-utils';
+import {
+  BulkMaturitySummary,
+  MaturitySummary,
+  Rank,
+} from '@backstage-community/plugin-tech-insights-maturity-common';
+
+import React from 'react';
+import { MaturitySummaryTable } from './MaturitySummaryTable';
+import { MaturityApi, maturityApiRef } from '../../api';
+
+const mockSystem: Entity = {
+  apiVersion: 'backstage.io/v1alpha1',
+  kind: 'System',
+  metadata: {
+    namespace: 'default',
+    name: 'mock-system',
+    title: 'Mock System',
+  },
+  relations: [
+    {
+      type: RELATION_HAS_PART,
+      targetRef: 'component:default/bingaux-sources',
+    },
+  ],
+  spec: {
+    owner: 'unknown',
+    domain: 'platform',
+  },
+};
+
+const mockComponent: Entity = {
+  apiVersion: 'backstage.io/v1alpha1',
+  kind: 'Component',
+  metadata: {
+    namespace: 'default',
+    name: 'bingaux-sources',
+    title: 'Bingaux Sources',
+    tags: ['python', 'csharp'],
+  },
+  spec: {
+    type: 'service',
+    lifecycle: 'production',
+    owner: 'team-119',
+    system: 'mock-system',
+  },
+  relations: [
+    {
+      targetRef: 'system:default/mock-system',
+      type: RELATION_PART_OF,
+    },
+  ],
+};
+
+describe('<MaturitySummaryTable />', () => {
+  afterEach(() => jest.resetAllMocks());
+
+  const summary: MaturitySummary = {
+    rank: Rank.Silver,
+    maxRank: Rank.Gold,
+    isMaxRank: false,
+    points: 100,
+    progress: {
+      percentage: 100,
+      totalChecks: 1,
+      passedChecks: 1,
+    },
+    rankProgress: {
+      percentage: 0,
+      totalChecks: 0,
+      passedChecks: 0,
+    },
+    areaSummaries: [
+      {
+        area: 'Ownership',
+        progress: {
+          percentage: 100,
+          totalChecks: 1,
+          passedChecks: 1,
+        },
+        rankProgress: {
+          percentage: 0,
+          totalChecks: 0,
+          passedChecks: 0,
+        },
+        rank: Rank.Silver,
+        maxRank: Rank.Gold,
+        isMaxRank: false,
+      },
+    ],
+  };
+
+  const bulkSummary: BulkMaturitySummary = [
+    { entity: 'component:default/bingaux-sources', summary },
+  ];
+
+  const catalogApi: Partial<CatalogApi> = {
+    getEntitiesByRefs: jest.fn().mockResolvedValue({ items: [mockComponent] }),
+  };
+
+  const scoringDataApi: Partial<MaturityApi> = {
+    getBulkMaturitySummary: jest.fn().mockResolvedValue(bulkSummary),
+  };
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('shows maturity summary table', async () => {
+    const { getByText, queryByText, getAllByAltText, getAllByTestId } =
+      await renderInTestApp(
+        <TestApiProvider
+          apis={[
+            [catalogApiRef, catalogApi],
+            [maturityApiRef, scoringDataApi],
+          ]}
+        >
+          <EntityProvider entity={mockSystem}>
+            <MaturitySummaryTable entities={[mockComponent]} />
+          </EntityProvider>
+        </TestApiProvider>,
+        {
+          mountedRoutes: {
+            '/catalog/:namespace/:kind/:name': entityRouteRef,
+          },
+        },
+      );
+    expect(queryByText(/Component Maturity/)).toBeInTheDocument(); // Title
+
+    expect(getByText(mockComponent.metadata.name)).toBeInTheDocument(); // Component name
+    expect(getAllByAltText(/Silver/)).toHaveLength(2); // Overall, Ownership
+    expect(queryByText(/100%/)).not.toBeInTheDocument(); // Overall progress hidden by default
+    expect(getByText(/Ownership/)).toBeInTheDocument(); // Area name
+
+    expect(queryByText(/0 Gold tasks left/)).toBeInTheDocument(); // Next Rank progress tip
+    expect(queryByText(/Max rank!/)).not.toBeInTheDocument(); // Next Rank progress tip
+    expect(getAllByTestId('progressbar').length).toBeGreaterThan(0); // Next Rank progress bar
+  });
+});
