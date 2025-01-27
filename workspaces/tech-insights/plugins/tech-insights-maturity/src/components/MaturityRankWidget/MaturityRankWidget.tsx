561d661c1598afd66d0b032891aacf48a6214107
Maica Ballangan <hballangan@mdsol.com>
2025-01-27 17:46:39 +0900

    New tech insights maturity plugin based on tech-insights  (#2464)
    * init tech-insights-maturity
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * add eslint
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * rename plugin export naming
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update readme
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * MaturityApiRef to extend TechInsightsApiRef
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * sanitize and refactor
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * export EntityMaturitySummaryContent
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * fix remaining issue
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update package dependencies
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * resolve use-react tsc issue
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * run prettier
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * generate build report
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * use workspace package
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * yarn fix
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * move tech-insights-maturity to  tech-insights workspace
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * add missing dependency
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * fix imported dependency
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor getMaturityScore
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove custom org implementations
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor MaturityLink
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove non-common plugin dependencies in maturity-common
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove unused components
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * remove unused components
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * Update workspaces/tech-insights/plugins/tech-insights-maturity/src/api/MaturityApi.ts
    
    Co-authored-by: Kurt King <kurtaking@gmail.com>
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update packages and report
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * update yarn packages
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * sync merge conflict
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * run yarn dedupe
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * generate api report md
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    * refactor maturityrank
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    
    ---------
    
    Signed-off-by: Maica Ballangan <hballangan@mdsol.com>
    Co-authored-by: Kurt King <kurtaking@gmail.com>

---
+++ workspaces/tech-insights/plugins/tech-insights-maturity/src/components/MaturityRankWidget/MaturityRankWidget.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import useAsyncRetry from 'react-use/lib/useAsync';
+
+import { Entity } from '@backstage/catalog-model';
+import { useApi } from '@backstage/core-plugin-api';
+import { maturityApiRef } from '../../api';
+import { MaturityRankAvatar } from '../MaturityRankAvatar';
+
+type Props = {
+  entity: Entity;
+};
+
+export const MaturityRankWidget = ({ entity }: Props) => {
+  const api = useApi(maturityApiRef);
+  const { value, loading } = useAsyncRetry(
+    async () => api.getMaturityRank(entity),
+    [api, entity],
+  );
+
+  if (loading || !value) return <></>;
+  return <MaturityRankAvatar entity={entity} value={value} variant="chip" />;
+};
