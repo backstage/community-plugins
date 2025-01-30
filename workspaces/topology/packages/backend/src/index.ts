c3d56ac63c611873c0dd9ad46a9016fd4aa03f8d
Jaivardhan Kumar <mailjai.vardhan@gmail.com>
2025-01-30 18:52:53 +0530

    v1.35.0 version bump for topology (#2608)
    Signed-off-by: Jaivardhan Kumar <mailjai.vardhan@gmail.com>
    Signed-off-by: Harmen Brinkman <harmen.brinkman@trimm.nl>

--- workspaces/topology/packages/backend/src/index.ts
+++ workspaces/topology/packages/backend/src/index.ts
@@ -53,7 +53,7 @@ backend.add(import('@backstage/plugin-search-backend/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
 
 // search collators
-backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
+backend.add(import('@backstage/plugin-search-backend-module-catalog'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
 // kubernetes
