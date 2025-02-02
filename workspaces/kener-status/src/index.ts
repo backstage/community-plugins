7b7bfa85d6dc9fc519e88edaa6e5fecffb463750
Naveen Alok <naveenalok@gmail.com>
2025-02-02 16:25:25 +0100

    feat(kener-status): add Kener status plugin for system monitoring
    Add new Kener status plugin that integrates with Kener to display uptime
    and status badges in Backstage system pages. The plugin provides:
    - System status card component
    - Configurable badge URLs through annotations
    - Responsive design for various screen sizes
    
    Signed-off-by: Naveen Alok <naveenalok@gmail.com>

---
+++ workspaces/kener-status/src/index.ts
@@ -0,0 +1,3 @@
+// plugins/kener-status/src/index.ts
+export { kenerStatusPlugin, rootRouteRef, EntityKenerStatusCard } from './plugin';
+export { KenerStatusPage } from './components/KenerStatusPage';
