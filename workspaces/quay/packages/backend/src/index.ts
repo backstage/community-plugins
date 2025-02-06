f910102032a3b23bfb72a7988e5e7dc9605c7523
Bryan Ramos <bramos@redhat.com>
2025-02-06 15:49:25 -0500

    Add back proxy package
    Allows local testing of the proxy
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- workspaces/quay/packages/backend/src/index.ts
+++ workspaces/quay/packages/backend/src/index.ts
@@ -36,6 +36,7 @@ backend.add(
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+backend.add(import('@backstage/plugin-proxy-backend'));
 
 // permission plugin
 backend.add(import('@backstage/plugin-permission-backend'));
