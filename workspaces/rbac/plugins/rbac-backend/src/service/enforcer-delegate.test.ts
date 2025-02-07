c08107a68b0eff722226ff94421e26450fa45601
Dominika Zemanovicova <dzemanov@redhat.com>
2025-02-07 14:33:21 +0100

    Introduce conditionalStorage to enforcerDelegate
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- workspaces/rbac/plugins/rbac-backend/src/service/enforcer-delegate.test.ts
+++ workspaces/rbac/plugins/rbac-backend/src/service/enforcer-delegate.test.ts
@@ -27,7 +27,10 @@ import {
 import { BackstageRoleManager } from '../role-manager/role-manager';
 import { EnforcerDelegate } from './enforcer-delegate';
 import { MODEL } from './permission-model';
-import { mockAuditorService } from '../../__fixtures__/mock-utils';
+import {
+  conditionalStorageMock,
+  mockAuditorService,
+} from '../../__fixtures__/mock-utils';
 
 // TODO: Move to 'catalogServiceMock' from '@backstage/plugin-catalog-node/testUtils'
 // once '@backstage/plugin-catalog-node' is upgraded
@@ -189,6 +192,7 @@ describe('EnforcerDelegate', () => {
     return new EnforcerDelegate(
       enf,
       mockAuditorService,
+      conditionalStorageMock,
       roleMetadataStorageMock,
       knex,
     );
