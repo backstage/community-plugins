13a53f756156ab170fd9aa5309b4a0531a2484af
Yash Oswal <yoswal@redhat.com>
2025-02-17 17:12:51 +0530

    feat(report-portal): add search collator module - create search bar for report portal plugin - update config.d.ts - move types to common package - upgrade workspace to v1.35
    Signed-off-by: Yash Oswal <yoswal@redhat.com>

--- workspaces/report-portal/plugins/report-portal-common/src/constants.ts
+++ workspaces/report-portal/plugins/report-portal-common/src/constants.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Backstage Authors
+ * Copyright 2025 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,13 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { createBackend } from '@backstage/backend-defaults';
 
-const backend = createBackend();
-
-backend.add(import('@backstage/plugin-auth-backend'));
-backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-backend.add(import('@backstage/plugin-catalog-backend'));
-backend.add(import('../src'));
-
-backend.start();
+/**
+ * Default filter used to make api calls
+ * @public
+ */
+export const DEFAULT_FILTER_TYPE = 'INTERNAL';
