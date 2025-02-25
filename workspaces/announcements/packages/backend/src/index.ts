25b695510f7e04fa3ae094c924403674afccc5ea
zeshanziya <zeshan.ziya@axelerant.com>
2025-02-25 07:49:23 +0530

    remove comment
    Signed-off-by: zeshanziya <zeshan.ziya@axelerant.com>

--- workspaces/announcements/packages/backend/src/index.ts
+++ workspaces/announcements/packages/backend/src/index.ts
@@ -36,9 +36,6 @@ backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 
 // permission plugin
 backend.add(import('@backstage/plugin-permission-backend'));
-// backend.add(
-//   import('@backstage/plugin-permission-backend-module-allow-all-policy'),
-// );
 backend.add(import('./permissionsPolicyExtension'));
 
 // search plugins
