6b7148a421ba4a9bf648b29083aa5602755c78f6
Christophe Augello <1112390+caugello@users.noreply.github.com>
2025-03-14 19:00:46 +0100

    feat(plugin): multi-source-security-viewer (#2901)
    Signed-off-by: Christophe Augello <caugello@redhat.com>

---
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/knip-report.md
@@ -0,0 +1,15 @@
+# Knip report
+
+## Unused dependencies (1)
+
+| Name                                                            | Location     | Severity |
+| :-------------------------------------------------------------- | :----------- | :------- |
+| @backstage-community/plugin-multi-source-security-viewer-common | package.json | error    |
+
+## Unused devDependencies (3)
+
+| Name                        | Location     | Severity |
+| :-------------------------- | :----------- | :------- |
+| @testing-library/user-event | package.json | error    |
+| @backstage/core-app-api     | package.json | error    |
+| msw                         | package.json | error    |
---
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/components/PipelineRunList/PipelineRunList.tsx
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import { debounce } from 'lodash';
+import { PipelineRunTable } from './PipelineRunTable';
+import { PipelineRunToolbar } from './PipelineRunToolbar';
+import { Box } from '@material-ui/core';
+import { PipelineRunResult } from '../../models/pipelineRunResult';
+
+type PipelineRunListProps = {
+  data: PipelineRunResult[] | null;
+  totalCount: number;
+  loading: boolean;
+  error: Error | null;
+  onUpdatePagination: (start: number, end: number) => void;
+};
+
+export const PipelineRunList: React.FC<PipelineRunListProps> = ({
+  data,
+  totalCount,
+  loading,
+  error,
+  onUpdatePagination,
+}) => {
+  const [namefilter, setNameFilter] = React.useState('');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [count, setCount] = React.useState(0);
+  const searchInputRef = React.useRef<HTMLInputElement | null>(null);
+
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+    onUpdatePagination(newPage, rowsPerPage);
+  };
+
+  const handleChangeRowsPerPage = (rows: number) => {
+    setRowsPerPage(rows);
+    setPage(0); // Set to zero when changing the number of rows
+    onUpdatePagination(0, page * rows + rows);
+  };
+
+  const filteredData = React.useMemo(() => {
+    return data?.filter((row: PipelineRunResult) =>
+      row?.id?.toString().toLocaleLowerCase().includes(namefilter),
+    );
+  }, [data, namefilter]);
+
+  const onSearch = debounce((n: string) => {
+    setNameFilter(n);
+  });
+
+  const filterClearAction = () => {
+    onSearch('');
+    if (searchInputRef.current) {
+      searchInputRef.current.value = '';
+    }
+  };
+
+  React.useEffect(() => {
+    if (!loading) {
+      // Keep count while fetching paginated data
+      setCount(prev => (totalCount > 0 ? totalCount : prev));
+    }
+  }, [loading, totalCount]);
+
+  return (
+    <React.Fragment>
+      <Box>
+        <Box>
+          <PipelineRunToolbar
+            onSearch={onSearch}
+            searchInputRef={searchInputRef}
+            totalCount={totalCount > 0 ? totalCount : count} // Initial count
+            page={page}
+            rowSize={[5, 10, 20]}
+            rowsPerPage={rowsPerPage}
+            handleChangePage={handleChangePage}
+            handleChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Box>
+        <Box>
+          <PipelineRunTable
+            data={filteredData ?? []}
+            filterInput={namefilter}
+            filterClearAction={filterClearAction}
+            isLoading={loading}
+            error={error}
+          />
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
+};
