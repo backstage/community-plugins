2c66dfb8bcec96461ae6d7c54af12f1b1fb42e93
Matt Knop <knopmatthew93@gmail.com>
2025-03-21 14:17:44 -0600

    migrating acs plugin (#2956)
    * migrating acs plugin
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * update to newer version of backstage
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * removing unused packages
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * remove unused packages and fixed typescript version
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * removed dockerfile
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * refers users to pre-req documentation
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updating docs, removing unused portions
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * removed markdown for unauth proxy config
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * adding prettier config
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * various package.json fixes
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * fixing readme at appropriate path
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updated annotation instructions
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updated codeowners
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * adding a codeowner
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updated yarn.lock
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updating installation instructions
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * updating yarn lock
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    
    * fixup! fix type errors
    
    Signed-off-by: Beth Griggs <bethanyngriggs@gmail.com>
    
    ---------
    
    Signed-off-by: Matt Knop <mknop@redhat.com>
    Signed-off-by: Beth Griggs <bethanyngriggs@gmail.com>
    Signed-off-by: Matt Knop <knopmatthew93@gmail.com>
    Co-authored-by: Beth Griggs <bethanyngriggs@gmail.com>

---
+++ workspaces/acs/plugins/acs/src/components/DataFilterComponent/EntitySelectComponent.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+interface EntitySelectProps {
+  options: Object;
+  setSelectedEntity: (value: string) => void;
+}
+
+export const EntitySelectComponent = ({
+  options,
+  setSelectedEntity,
+}: EntitySelectProps) => {
+  /* eslint @typescript-eslint/no-shadow: ["error", { "allow": ["isOpen"] }]*/
+  const [selected, setSelected] = React.useState<string>(
+    Object.keys(options)[0],
+  );
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSelected(event.target.value as string);
+    setSelectedEntity(event.target.value as string);
+  };
+
+  return (
+    <Select
+      id="entity"
+      value={selected}
+      onChange={handleChange}
+      displayEmpty
+      inputProps={{ 'aria-label': 'entity' }}
+    >
+      {Object.entries(options).map(([key, _]) => (
+        <MenuItem value={key}>{key}</MenuItem>
+      ))}
+    </Select>
+  );
+};
+
+export default EntitySelectComponent;
