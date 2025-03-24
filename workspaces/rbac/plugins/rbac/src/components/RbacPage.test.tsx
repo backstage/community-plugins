ae8dfe3e4f0b97f13454b64476ca41cb140fad55
Patrick <pknight@redhat.com>
2025-03-24 11:23:14 -0400

    feat(rbac): hide RBACPage behind read permission again
    Signed-off-by: Patrick <pknight@redhat.com>

--- workspaces/rbac/plugins/rbac/src/components/RbacPage.test.tsx
+++ workspaces/rbac/plugins/rbac/src/components/RbacPage.test.tsx
@@ -15,17 +15,15 @@
  */
 import React from 'react';
 
-import {
-  RequirePermission,
-  usePermission,
-} from '@backstage/plugin-permission-react';
-import { renderInTestApp } from '@backstage/test-utils';
+import { renderInTestApp, TestApiProvider } from '@backstage/test-utils';
 
 import { screen } from '@testing-library/react';
 
 import { useCheckIfLicensePluginEnabled } from '../hooks/useCheckIfLicensePluginEnabled';
 import { useRoles } from '../hooks/useRoles';
 import { RbacPage } from './RbacPage';
+
+import { RBACAPI, rbacApiRef } from '../api/RBACBackendClient';
 
 jest.mock('@backstage/plugin-permission-react', () => ({
   usePermission: jest.fn(),
@@ -40,10 +38,6 @@ jest.mock('../hooks/useCheckIfLicensePluginEnabled', () => ({
   useCheckIfLicensePluginEnabled: jest.fn(),
 }));
 
-const mockUsePermission = usePermission as jest.MockedFunction<
-  typeof usePermission
->;
-
 const mockUseRoles = useRoles as jest.MockedFunction<typeof useRoles>;
 
 const mockUseCheckIfLicensePluginEnabled =
@@ -51,14 +45,26 @@ const mockUseCheckIfLicensePluginEnabled =
     typeof useCheckIfLicensePluginEnabled
   >;
 
-const RequirePermissionMock = RequirePermission as jest.MockedFunction<
-  typeof RequirePermission
->;
+// Added
+let useAsyncMockResult: { loading: boolean; value?: { status: string } } = {
+  loading: false,
+  value: { status: 'Authorized' },
+};
+
+const mockRbacApi: jest.Mocked<Partial<RBACAPI>> = {
+  getUserAuthorization: jest.fn().mockImplementation(() => useAsyncMockResult),
+};
+
+jest.mock('react-use', () => ({
+  ...jest.requireActual('react-use'),
+  useAsync: jest.fn().mockImplementation((fn: any, _deps: any) => {
+    fn();
+    return useAsyncMockResult;
+  }),
+}));
 
 describe('RbacPage', () => {
   it('should render if authorized', async () => {
-    RequirePermissionMock.mockImplementation(props => <>{props.children}</>);
-    mockUsePermission.mockReturnValue({ loading: false, allowed: true });
     mockUseRoles.mockReturnValue({
       loading: true,
       data: [],
@@ -79,23 +85,40 @@ describe('RbacPage', () => {
         name: '',
       },
     });
-    await renderInTestApp(<RbacPage />);
+
+    await renderInTestApp(
+      <TestApiProvider apis={[[rbacApiRef, mockRbacApi]]}>
+        <RbacPage />
+      </TestApiProvider>,
+    );
     expect(screen.getByText('RBAC')).toBeInTheDocument();
   });
 
   it('should not render if not authorized', async () => {
-    RequirePermissionMock.mockImplementation(_props => <>Not Found</>);
-    mockUsePermission.mockReturnValue({ loading: false, allowed: false });
+    useAsyncMockResult = {
+      loading: false,
+      value: { status: 'Unauthorized' },
+    };
 
-    await renderInTestApp(<RbacPage />);
-    expect(screen.getByText('Not Found')).toBeInTheDocument();
+    await renderInTestApp(
+      <TestApiProvider apis={[[rbacApiRef, mockRbacApi]]}>
+        <RbacPage />
+      </TestApiProvider>,
+    );
+    expect(screen.getByText('ERROR : Not Found')).toBeInTheDocument();
   });
 
   it('should not render if loading', async () => {
-    RequirePermissionMock.mockImplementation(_props => null);
-    mockUsePermission.mockReturnValue({ loading: false, allowed: false });
+    useAsyncMockResult = {
+      loading: true,
+      value: undefined,
+    };
 
-    const { queryByText } = await renderInTestApp(<RbacPage />);
+    const { queryByText } = await renderInTestApp(
+      <TestApiProvider apis={[[rbacApiRef, mockRbacApi]]}>
+        <RbacPage />
+      </TestApiProvider>,
+    );
     expect(queryByText('Not Found')).not.toBeInTheDocument();
     expect(queryByText('RBAC')).not.toBeInTheDocument();
   });
