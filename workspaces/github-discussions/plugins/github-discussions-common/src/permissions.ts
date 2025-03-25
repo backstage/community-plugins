f0a393f4793304207a75e99f5b961e3bf8c101a1
Min Kim <minkimcello@gmail.com>
2025-03-25 07:03:07 -0400

    Add visibility permissions for GitHub Discussions Search Collator (#2574)
    * Add visibility permissions for github-discussions
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Add changesets
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Extract and export permission from the common library
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    Squash
    
    * Rename permissions to githubDiscussionsReadPermission
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Rewrite changeset description
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Relocate types in discussions-common
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Dedupe yarn.lock
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    * Build API reports
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>
    
    ---------
    
    Signed-off-by: Min Kim <minkimcello@gmail.com>

--- workspaces/github-discussions/plugins/github-discussions-common/src/permissions.ts
+++ workspaces/github-discussions/plugins/github-discussions-common/src/permissions.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Backstage Authors
+ * Copyright 2025 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { createPermission } from '@backstage/plugin-permission-common';
 
-export { githubDiscussionsReadPermission } from './permissions';
-export * from './types';
+/**
+ * @public
+ */
+export const githubDiscussionsReadPermission = createPermission({
+  name: 'github.discussions.read',
+  attributes: {
+    action: 'read',
+  },
+});
