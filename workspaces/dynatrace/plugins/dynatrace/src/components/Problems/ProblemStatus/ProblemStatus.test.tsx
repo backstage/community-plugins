efe50da1130587e5dd9e8b37f02910aa72e13f86
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 04:31:01 +0200

    dynatrace - version:bump to v1.38.1 (#3700)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/dynatrace/plugins/dynatrace/src/components/Problems/ProblemStatus/ProblemStatus.test.tsx
+++ workspaces/dynatrace/plugins/dynatrace/src/components/Problems/ProblemStatus/ProblemStatus.test.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { ProblemStatus } from './ProblemStatus';
 import { renderInTestApp } from '@backstage/test-utils';
 
