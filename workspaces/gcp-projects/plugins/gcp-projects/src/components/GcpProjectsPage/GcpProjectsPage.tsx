f7d2349b881490c9c1f6c804f3b7f0503954697a
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 04:25:03 +0200

    gcp-projects - version:bump to v1.38.1 (#3705)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>
    Signed-off-by: Jarek <jaroslaw.gajewski@atos.net>

--- workspaces/gcp-projects/plugins/gcp-projects/src/components/GcpProjectsPage/GcpProjectsPage.tsx
+++ workspaces/gcp-projects/plugins/gcp-projects/src/components/GcpProjectsPage/GcpProjectsPage.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { NewProjectPage } from '../NewProjectPage';
 import { ProjectDetailsPage } from '../ProjectDetailsPage';
