afc84e9b90fa381791252c48207ff7ff17c3ea5c
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 04:24:47 +0200

    github-issues - version:bump to v1.38.1 (#3707)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/github-issues/plugins/github-issues/src/components/GithubIssues/IssueCard/CommentsCount.tsx
+++ workspaces/github-issues/plugins/github-issues/src/components/GithubIssues/IssueCard/CommentsCount.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import { ChatIcon } from '@backstage/core-components';
 import Box from '@material-ui/core/Box';
 import Badge from '@material-ui/core/Badge';
