def251458fd7672ab89c2111b7603072d053c38f
Andre Wanlin <awanlin@spotify.com>
2025-04-17 20:33:12 -0500

    Manual changes for release
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/gcp-projects/plugins/gcp-projects/src/components/NewProjectPage/NewProjectPage.tsx
+++ workspaces/gcp-projects/plugins/gcp-projects/src/components/NewProjectPage/NewProjectPage.tsx
@@ -17,7 +17,7 @@
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {
   Content,
   ContentHeader,
