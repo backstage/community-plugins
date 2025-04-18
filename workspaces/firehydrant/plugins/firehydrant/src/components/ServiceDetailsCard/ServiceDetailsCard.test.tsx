e81cc2bae9eaaabc58b6e82ded640e068d4f4324
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 04:24:55 +0200

    firehydrant - version:bump to v1.38.1 (#3706)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>
    Signed-off-by: Jarek <jaroslaw.gajewski@atos.net>

--- workspaces/firehydrant/plugins/firehydrant/src/components/ServiceDetailsCard/ServiceDetailsCard.test.tsx
+++ workspaces/firehydrant/plugins/firehydrant/src/components/ServiceDetailsCard/ServiceDetailsCard.test.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { ApiProvider } from '@backstage/core-app-api';
 import { fireHydrantApiRef } from '../../api';
 import { screen } from '@testing-library/react';
