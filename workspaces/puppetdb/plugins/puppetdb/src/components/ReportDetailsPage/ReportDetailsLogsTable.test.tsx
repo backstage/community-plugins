92902f173e7a8c26fe81eec0fd64ddfb9c078fdb
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 18:14:03 +0200

    puppetdb - version:bump to v1.38.1 (#3740)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/puppetdb/plugins/puppetdb/src/components/ReportDetailsPage/ReportDetailsLogsTable.test.tsx
+++ workspaces/puppetdb/plugins/puppetdb/src/components/ReportDetailsPage/ReportDetailsLogsTable.test.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { ReportDetailsLogsTable } from './ReportDetailsLogsTable';
 import { renderInTestApp, TestApiRegistry } from '@backstage/test-utils';
 import { puppetDbApiRef, PuppetDbReportLog } from '../../api';
