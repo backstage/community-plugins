b36ea2d1085731886e0210ee20693bde0450eacb
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 18:14:31 +0200

    splunk - version:bump to v1.38.1 (#3737)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>
    Signed-off-by: Jarek <jaroslaw.gajewski@atos.net>

--- workspaces/splunk/plugins/splunk-on-call/src/components/Escalation/EscalationPolicy.tsx
+++ workspaces/splunk/plugins/splunk-on-call/src/components/Escalation/EscalationPolicy.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
--- workspaces/splunk/plugins/splunk-on-call/src/components/Incident/IncidentEmptyState.tsx
+++ workspaces/splunk/plugins/splunk-on-call/src/components/Incident/IncidentEmptyState.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import EmptyStateImage from '../../assets/emptystate.svg';
