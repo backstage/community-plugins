3f4cf0c439fa77fe310a232570a6eeae59d9bcb1
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 18:13:53 +0200

    shortcuts - version:bump to v1.38.1 (#3741)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    * Updated command
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/shortcuts/plugins/shortcuts/src/EditShortcut.test.tsx
+++ workspaces/shortcuts/plugins/shortcuts/src/EditShortcut.test.tsx
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import React from 'react';
 import { screen, fireEvent, waitFor } from '@testing-library/react';
 import { EditShortcut } from './EditShortcut';
 import { Shortcut } from './types';
