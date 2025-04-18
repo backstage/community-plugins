2f62dc9d0986dddc5ac7d4cfca5979904a819c06
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-18 04:25:47 +0200

    gcalendar - version:bump to v1.38.1 (#3701)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>
    Signed-off-by: Jarek <jaroslaw.gajewski@atos.net>

--- workspaces/gcalendar/plugins/gcalendar/dev/index.tsx
+++ workspaces/gcalendar/plugins/gcalendar/dev/index.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { Content, Page } from '@backstage/core-components';
 import { googleAuthApiRef } from '@backstage/core-plugin-api';
 import { createDevApp } from '@backstage/dev-utils';
