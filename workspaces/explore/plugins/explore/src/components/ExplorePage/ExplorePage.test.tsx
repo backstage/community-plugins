3cc0e3d58947aca6ae666d6580e8ccf225eef02b
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-04-22 23:38:03 +0200

    explore - version:bump to v1.38.1 (#3812)
    * v1.38.1 version bump
    
    * Manual changes for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/explore/plugins/explore/src/components/ExplorePage/ExplorePage.test.tsx
+++ workspaces/explore/plugins/explore/src/components/ExplorePage/ExplorePage.test.tsx
@@ -15,7 +15,6 @@
  */
 
 import { renderInTestApp } from '@backstage/test-utils';
-import React from 'react';
 import { useOutlet } from 'react-router-dom';
 import { ExplorePage } from './ExplorePage';
 
