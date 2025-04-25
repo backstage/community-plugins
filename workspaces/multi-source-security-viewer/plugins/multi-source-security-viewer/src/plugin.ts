f753c4c568f1314209139cd6874bf78533f31bba
Bryan Ramos <bramos@redhat.com>
2025-04-25 13:46:42 -0400

    Add accessibility changes
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/plugin.ts
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/plugin.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
 import '@patternfly/react-core/dist/styles/base-no-reset.css';
 
 import {
