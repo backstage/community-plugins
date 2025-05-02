0608551144c11feb7d93ad11f08bb1d06011558b
Andre Wanlin <awanlin@spotify.com>
2025-05-02 15:24:51 -0500

    Manual changes for release
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/azure-storage-explorer/plugins/azure-storage/src/components/AzureStoragePage/AzureStorageContent.tsx
+++ workspaces/azure-storage-explorer/plugins/azure-storage/src/components/AzureStoragePage/AzureStorageContent.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Typography, Grid, makeStyles } from '@material-ui/core';
 import SubdirectoryArrowLeftIcon from '@material-ui/icons/SubdirectoryArrowLeft';
 import GetAppIcon from '@material-ui/icons/GetApp';
