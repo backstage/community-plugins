549723f2314297713855965231fb9d9191650390
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-05-03 16:15:39 +0200

    blackduck - version:bump to v1.38.1 (#3851)
    * v1.38.1 version bump
    
    * Manual changes for the Release
    
    Signed-off-by: Deepankumar Loganathan <deepan0433@gmail.com>
    
    ---------
    
    Signed-off-by: Deepankumar Loganathan <deepan0433@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Deepankumar Loganathan <deepan0433@gmail.com>

--- workspaces/blackduck/plugins/blackduck/src/components/BlackDuckCard/RiskCard.tsx
+++ workspaces/blackduck/plugins/blackduck/src/components/BlackDuckCard/RiskCard.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { blackduckApiRef } from '../../api';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { useApi } from '@backstage/core-plugin-api';
