8b665f95dc22a444b0e43c227fb8d7cb8e1ce9b4
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-05-09 22:17:46 +0200

    analytics - version:bump to v1.38.1 (#3845)
    * v1.38.1 version bump
    
    * Manual change for release
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/analytics/plugins/analytics-module-matomo/dev/index.tsx
+++ workspaces/analytics/plugins/analytics-module-matomo/dev/index.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
 import { MatomoAnalyticsApi } from '../src';
 import { Playground } from './Playground';
