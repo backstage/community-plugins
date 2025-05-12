7492d6cc2688ab251d8c77b83b35bdbb912e22a6
Christoph Jerolimov <jerolimov+git@redhat.com>
2025-05-13 00:14:03 +0200

    fix(acr): remove react import
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- workspaces/acr/plugins/acr/dev/index.tsx
+++ workspaces/acr/plugins/acr/dev/index.tsx
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
-
 import { createDevApp } from '@backstage/dev-utils';
 import { TestApiProvider } from '@backstage/test-utils';
 import { Page, Header, TabbedLayout } from '@backstage/core-components';
