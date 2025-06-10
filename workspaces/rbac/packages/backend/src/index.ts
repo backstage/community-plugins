e0c94175e283b1f6d1363044ae74b9b2502ffb29
Patrick <pknight@redhat.com>
2025-06-10 09:27:31 -0400

    fix(rbac): add optional oidc auth provider to workspace backstage app
    Signed-off-by: Patrick <pknight@redhat.com>

--- workspaces/rbac/packages/backend/src/index.ts
+++ workspaces/rbac/packages/backend/src/index.ts
@@ -29,6 +29,7 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+backend.add(import('@backstage/plugin-auth-backend-module-oidc-provider'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
