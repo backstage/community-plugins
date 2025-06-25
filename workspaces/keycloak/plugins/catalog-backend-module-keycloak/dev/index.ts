5d46f9e8bb37d52581191a60444f246d9bc7e52e
Jessica He <jhe@redhat.com>
2025-06-25 15:50:38 -0400

    add plugin-catalog-backend-module-logs
    Signed-off-by: Jessica He <jhe@redhat.com>

--- workspaces/keycloak/plugins/catalog-backend-module-keycloak/dev/index.ts
+++ workspaces/keycloak/plugins/catalog-backend-module-keycloak/dev/index.ts
@@ -21,6 +21,10 @@ const backend = createBackend();
 
 // api endpoints from here: https://github.com/backstage/backstage/blob/master/plugins/catalog-backend/src/service/createRouter.ts
 backend.add(import('@backstage/plugin-catalog-backend'));
+
+// Subscribe to catalog errors https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
+backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+
 backend.add(catalogModuleKeycloakEntityProvider);
 
 backend.start();
