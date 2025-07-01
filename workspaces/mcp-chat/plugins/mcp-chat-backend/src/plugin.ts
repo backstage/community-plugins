280b63bdce640ad59f7664ba9157fe82ec17c363
Lucifergene <47265560+Lucifergene@users.noreply.github.com>
2025-07-01 22:58:21 +0530

    renamed plugin
    Signed-off-by: Lucifergene <47265560+Lucifergene@users.noreply.github.com>

---
+++ workspaces/mcp-chat/plugins/mcp-chat-backend/src/plugin.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  coreServices,
+  createBackendPlugin,
+} from '@backstage/backend-plugin-api';
+import { createRouter } from './router';
+import { MCPClientServiceImpl } from './services';
+
+/**
+ * mcpChatPlugin backend plugin
+ *
+ * @public
+ */
+export const mcpChatPlugin = createBackendPlugin({
+  pluginId: 'mcp-chat',
+  register(env) {
+    env.registerInit({
+      deps: {
+        logger: coreServices.logger,
+        httpAuth: coreServices.httpAuth,
+        config: coreServices.rootConfig,
+        httpRouter: coreServices.httpRouter,
+      },
+      async init({ logger, httpAuth, httpRouter, config }) {
+        const mcpClientService = new MCPClientServiceImpl({
+          logger,
+          config,
+        });
+
+        httpRouter.use(
+          await createRouter({
+            httpAuth,
+            mcpClientService,
+            config,
+          }),
+        );
+        httpRouter.addAuthPolicy({
+          path: '/',
+          allow: 'unauthenticated',
+        });
+      },
+    });
+  },
+});
