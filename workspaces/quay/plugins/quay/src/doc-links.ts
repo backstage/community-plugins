03417702b2044dd9c2365c60b003bcbb5deb5f2b
Bryan Ramos <30709212+CryptoRodeo@users.noreply.github.com>
2025-07-03 09:54:40 -0400

    feat(quay): enhance debugging access issues (#4370)
    * feat(quay): enhance debugging access issues
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    * Store doc links as const
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    * Move error logger up
    
    Should log before any error is thrown.
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    * Update test scenario
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    ---------
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

---
+++ workspaces/quay/plugins/quay/src/doc-links.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export const DOC_LINKS = {
+  AUTH_TOKEN_GUIDE:
+    'https://docs.redhat.com/en/documentation/red_hat_quay/3/html-single/red_hat_quay_api_guide/index#creating-oauth-access-token',
+  BACKEND_CONFIGURATION_GUIDE:
+    'https://github.com/backstage/community-plugins/tree/main/workspaces/quay/plugins/quay-backend#app-config',
+} as const;
