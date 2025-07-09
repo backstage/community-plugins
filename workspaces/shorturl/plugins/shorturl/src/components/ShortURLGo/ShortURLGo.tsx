741968feaa3a81e7d3fdb89ec74315c9b661f44a
Volodymyr Kocherhin <47826449+vkvkvkk@users.noreply.github.com>
2025-07-09 16:25:08 -0400

    Refactored ShortURLGo to use useAsync from react-hookz/web
    Signed-off-by: Volodymyr Kocherhin <47826449+vkvkvkk@users.noreply.github.com>

--- workspaces/shorturl/plugins/shorturl/src/components/ShortURLGo/ShortURLGo.tsx
+++ workspaces/shorturl/plugins/shorturl/src/components/ShortURLGo/ShortURLGo.tsx
@@ -13,9 +13,9 @@ import {
   ErrorPanel,
   Progress,
 } from '@backstage/core-components';
+import { useAsync } from '@react-hookz/web';
 
 export const ShortURLGo = () => {
-  const [redirectError, setRedirectError] = React.useState(new Error());
   const fetchApi = useApi(fetchApiRef);
   const identityApi = useApi(identityApiRef);
   const discoveryApi = useApi(discoveryApiRef);
@@ -26,44 +26,37 @@ export const ShortURLGo = () => {
   );
   const shortUrl = window.location.href.split('/go/')[1];
 
+  const [{ status, error }, { execute }] = useAsync(async () => {
+    if (!shortUrl || shortUrl === 'undefined') {
+      throw new Error('Undefined ID found in URL');
+    }
+
+    const response = await shortURLApi.getRedirectURL(shortUrl);
+    const responseJson = await response.json();
+
+    if (response.status === 200) {
+      window.location.href = responseJson.redirectUrl;
+    } else if (response.status === 404) {
+      throw new Error(`No redirection found for path ${shortUrl}`);
+    } else {
+      throw new Error(
+        `StatusCode: ${response.status}, Message: ${
+          responseJson?.message || response.statusText
+        }`,
+      );
+    }
+  });
+
   useEffect(() => {
-    const fetchData = async () => {
-      if (!shortUrl || shortUrl === 'undefined') {
-        setRedirectError(new Error('Undefined ID found in URL'));
-        return;
-      }
-      try {
-        const response = await shortURLApi.getRedirectURL(shortUrl);
-        const responseJson = await response.json();
-        if (response.status === 200) {
-          window.location.href = responseJson.redirectUrl;
-          return;
-        } else if (response.status === 404) {
-          setRedirectError(
-            new Error(`No redirection found for path ${shortUrl}`),
-          );
-        } else {
-          setRedirectError(
-            new Error(
-              `StatusCode: ${response.status}, Message: ${response.json}`,
-            ),
-          );
-        }
-      } catch (error) {
-        setRedirectError(
-          new Error(`Failed to fetch redirection URL: ${error}`),
-        );
-      }
-    };
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [shortUrl]);
+    execute();
+  });
 
   return (
     <Content>
-      {(redirectError.message === '' && <Progress />) || (
+      {status === 'loading' && <Progress />}
+      {status === 'error' && error && (
         <Box>
-          <ErrorPanel title="Redirection failure" error={redirectError} />
+          <ErrorPanel title="Redirection failure" error={error} />
           <BottomLink title="Check existing ShortURLs" link="/shorturl" />
         </Box>
       )}
