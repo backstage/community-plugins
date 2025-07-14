e7a900cc48f95cd42853aaffdeb00d231bc50e72
Volodymyr Kocherhin <47826449+vkvkvkk@users.noreply.github.com>
2025-07-14 17:06:34 -0400

    Extracted a hook from ShortURLCreate component
    Signed-off-by: Volodymyr Kocherhin <47826449+vkvkvkk@users.noreply.github.com>

--- workspaces/shorturl/plugins/shorturl/src/components/ShortURLPage/ShortURLCreate.tsx
+++ workspaces/shorturl/plugins/shorturl/src/components/ShortURLPage/ShortURLCreate.tsx
@@ -1,14 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import {
-  useApi,
-  alertApiRef,
-  discoveryApiRef,
-  configApiRef,
-  fetchApiRef,
-  identityApiRef,
-} from '@backstage/core-plugin-api';
-import { DefaultShortURLApi } from '../../api';
-import { useAsync } from '@react-hookz/web';
+import React from 'react';
 import {
   Box,
   Button,
@@ -18,110 +8,15 @@ import {
   TextField,
 } from '@material-ui/core';
 import { Link } from '@backstage/core-components';
+import { useCreateShortUrl } from '../../hooks/useCreateShortUrl';
 
 interface ShortURLCreateProps {
   onCreate: () => void;
 }
 
-const validateUrl = (url: string): boolean => {
-  try {
-    // eslint-disable-next-line no-new
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
 export const ShortURLCreate: React.FC<ShortURLCreateProps> = ({ onCreate }) => {
-  const alertApi = useApi(alertApiRef);
-  const fetchApi = useApi(fetchApiRef);
-  const configApi = useApi(configApiRef);
-  const identityApi = useApi(identityApiRef);
-  const discoveryApi = useApi(discoveryApiRef);
-  const shortURLApi = new DefaultShortURLApi(
-    fetchApi,
-    discoveryApi,
-    identityApi,
-  );
-
-  const [longUrl, setLongUrl] = useState('');
-  const [shortUrl, setShortUrl] = useState('');
-
-  const [{ result: baseUrl }, { execute: fetchBaseUrl }] = useAsync(
-    async () => {
-      return configApi.getString('app.baseUrl');
-    },
-  );
-
-  useEffect(() => {
-    fetchBaseUrl();
-  }, [fetchBaseUrl]);
-
-  const copyToClipboard = useCallback(
-    async (text: string) => {
-      try {
-        await navigator.clipboard.writeText(text);
-        alertApi.post({
-          message: 'Short URL copied to clipboard',
-          display: 'transient',
-        });
-      } catch {
-        alertApi.post({
-          message: `Failed to copy short URL to clipboard`,
-          severity: 'error',
-        });
-      }
-    },
-    [alertApi],
-  );
-
-  const handleCopyClick = async () => {
-    copyToClipboard(shortUrl);
-  };
-
-  const handleCreateClick = async () => {
-    if (!validateUrl(longUrl)) {
-      alertApi.post({
-        message: 'Invalid URL',
-        severity: 'error',
-      });
-      return;
-    }
-
-    try {
-      const res = await shortURLApi.createOrRetrieveShortUrl({
-        fullUrl: longUrl,
-        usageCount: 0,
-      });
-
-      if (res.status !== 200 && res.status !== 201) {
-        throw new Error(`API returned status ${res.status}`);
-      }
-
-      const json = await res.json();
-
-      if (json.status === 'ok') {
-        const newLink = `${baseUrl}/go/${json.shortUrl}`;
-        setShortUrl(newLink);
-      } else {
-        throw new Error('Unexpected response from server');
-      }
-    } catch (err: any) {
-      alertApi.post({
-        message: `Failed to create short URL: ${err.message || err}`,
-        severity: 'error',
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (shortUrl) {
-      copyToClipboard(shortUrl);
-      onCreate();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [shortUrl]);
+  const { longUrl, setLongUrl, shortUrl, createClick, copyToClipboard } =
+    useCreateShortUrl(onCreate);
 
   return (
     <Card>
@@ -138,11 +33,7 @@ export const ShortURLCreate: React.FC<ShortURLCreateProps> = ({ onCreate }) => {
             />
           </Grid>
           <Grid item md={2} xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleCreateClick}
-            >
+            <Button variant="contained" color="primary" onClick={createClick}>
               Create URL
             </Button>
           </Grid>
@@ -154,9 +45,7 @@ export const ShortURLCreate: React.FC<ShortURLCreateProps> = ({ onCreate }) => {
                 </Link>
               </Grid>
               <Grid item md={2} xs={12}>
-                <Button variant="contained" onClick={handleCopyClick}>
-                  Copy
-                </Button>
+                <Button onClick={() => copyToClipboard(shortUrl)}>Copy</Button>
               </Grid>
             </>
           )}
