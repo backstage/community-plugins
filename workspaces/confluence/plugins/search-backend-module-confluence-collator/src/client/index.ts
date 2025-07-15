2343a10febd621319df7f79e31fb34f666fa3674
Gabriel Dugny <gabriel.dugny@believe.com>
2025-07-15 18:48:54 +0200

    feat: Split Collator & Client, add cache to client
    Signed-off-by: Gabriel Dugny <gabriel.dugny@believe.com>
    
    # Conflicts:
    #	workspaces/confluence/plugins/search-backend-module-confluence-collator/src/collators/ConfluenceCollatorFactory.ts
    
    # Conflicts:
    #	workspaces/confluence/plugins/search-backend-module-confluence-collator/README.md
    
    # Conflicts:
    #	workspaces/confluence/plugins/search-backend-module-confluence-collator/package.json
    #	workspaces/confluence/yarn.lock

--- workspaces/confluence/plugins/search-backend-module-confluence-collator/src/client/index.ts
+++ workspaces/confluence/plugins/search-backend-module-confluence-collator/src/client/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Backstage Authors
+ * Copyright 2025 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,10 @@
  * limitations under the License.
  */
 export {
-  type ConfluenceCollatorFactoryOptions,
-  type IndexableAncestorRef,
-  type IndexableConfluenceDocument,
-  ConfluenceCollatorFactory,
-} from './ConfluenceCollatorFactory';
+  ConfluenceClient,
+  type ConfluenceClientOptions,
+  type ConfluenceDocument,
+  type ConfluenceDocumentMetadata,
+  type ConfluenceDocumentList,
+  type DocumentMetadata,
+} from './ConfluenceClient';
