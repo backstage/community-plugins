e3a92583e061f5ce165bc9f58b26a343032985be
Oleksandr Andriienko <oandriie@redhat.com>
2025-08-27 18:05:12 +0300

    feat(rbac): fix preferPermissionPolicy logic Make preferPermissionPolicy option working for permission policy with deny value.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- workspaces/rbac/plugins/rbac-backend/src/policies/permission-policy.test.ts
+++ workspaces/rbac/plugins/rbac-backend/src/policies/permission-policy.test.ts
@@ -1810,12 +1810,19 @@ describe('Policy checks for conditional policies', () => {
   });
 });
 
-describe('Policy checks with policyDecisionPrecedence config', () => {
-  const policies = [
+describe('Policy checks with preferPermissionPolicy config', () => {
+  const allowReadAndCreatepolicies = [
     // allow read for all resources
     ['role:default/all_resource_reader', 'catalog-entity', 'read', 'allow'],
     ['role:default/all_resource_reader', 'catalog-entity', 'create', 'allow'],
   ];
+
+  const allowCreateButDenyReadpolicies = [
+    // deny read for all resources
+    ['role:default/all_resource_reader', 'catalog-entity', 'read', 'deny'],
+    ['role:default/all_resource_reader', 'catalog-entity', 'create', 'allow'],
+  ];
+
   const groupPolicies = [
     ['user:default/mike', 'role:default/all_resource_reader'],
     ['user:default/mike', 'role:default/owned_resource_reader'],
@@ -1839,13 +1846,13 @@ describe('Policy checks with policyDecisionPrecedence config', () => {
     },
   ];
 
-  it('should allow create when policyDecisionPrecedence is basic (permission policy first)', async () => {
+  it('should allow "catalog read operation" when preferPermissionPolicy is true (permission policy first) and read policy has "allow" value', async () => {
     const config = newConfig(undefined, undefined, undefined, 'basic');
     const adapter = await newAdapter(config);
     const enfDelegate = await newEnforcerDelegate(
       adapter,
       config,
-      policies,
+      allowReadAndCreatepolicies,
       groupPolicies,
     );
     const policy = await newPermissionPolicy(config, enfDelegate);
@@ -1866,13 +1873,40 @@ describe('Policy checks with policyDecisionPrecedence config', () => {
     expect(decision).toStrictEqual({ result: AuthorizeResult.ALLOW });
   });
 
-  it('should NOT allow create when policyDecisionPrecedence is conditional by default (conditional policy first)', async () => {
-    const config = newConfig();
-    const adapter = await newAdapter(newConfig());
+  it('should deny "catalog read operation" when preferPermissionPolicy is true (permission policy first) and read policy has "deny" value', async () => {
+    const config = newConfig(undefined, undefined, undefined, 'basic');
+    const adapter = await newAdapter(config);
     const enfDelegate = await newEnforcerDelegate(
       adapter,
       config,
-      policies,
+      allowCreateButDenyReadpolicies,
+      groupPolicies,
+    );
+    const policy = await newPermissionPolicy(config, enfDelegate);
+
+    // Mock conditionalStorage to return a conditional ALLOW for owned-reader
+    (
+      conditionalStorageMock.filterConditions as jest.Mock
+    ).mockResolvedValueOnce(conditionalPolicy);
+
+    const decision = await policy.handle(
+      newPolicyQueryWithResourcePermission(
+        'catalog.entity.read',
+        'catalog-entity',
+        'read',
+      ),
+      newPolicyQueryUser('user:default/mike', ['user:default/mike']), // user is owner
+    );
+    expect(decision).toStrictEqual({ result: AuthorizeResult.DENY });
+  });
+
+  it('should NOT allow read when preferPermissionPolicy is false by default (conditional policy first)', async () => {
+    const config = newConfig();
+    const adapter = await newAdapter(config);
+    const enfDelegate = await newEnforcerDelegate(
+      adapter,
+      config,
+      allowReadAndCreatepolicies,
       groupPolicies,
     );
     const policy = await newPermissionPolicy(config, enfDelegate);
