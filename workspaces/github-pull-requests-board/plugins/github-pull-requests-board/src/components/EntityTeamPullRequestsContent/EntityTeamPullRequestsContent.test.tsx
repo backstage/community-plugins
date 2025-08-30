2cea0d09393d4f8b88e3acddab162650d85ed092
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-08-30 22:34:35 +0200

    github-pull-requests-board - version:bump to v1.42.3 (#5087)
    * v1.42.3 version bump
    
    * Manual changes for last few releases and use yarn plugin
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    * Updated API Reports and prettier
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    * Prettier
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    
    ---------
    
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Andre Wanlin <awanlin@spotify.com>

--- workspaces/github-pull-requests-board/plugins/github-pull-requests-board/src/components/EntityTeamPullRequestsContent/EntityTeamPullRequestsContent.test.tsx
+++ workspaces/github-pull-requests-board/plugins/github-pull-requests-board/src/components/EntityTeamPullRequestsContent/EntityTeamPullRequestsContent.test.tsx
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React from 'react';
 import { EntityTeamPullRequestsContent } from '../EntityTeamPullRequestsContent';
 import { PullRequestsColumn, Status } from '../../utils/types';
 import { render } from '@testing-library/react';
