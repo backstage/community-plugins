ab2429d156e3d100f5eea946f8d6fdb00ba9a3d6
Kaushalendra-Singh-007 <singhkush0307@gmail.com>
2025-09-09 00:14:49 +0530

    Added change set, updated config.d.ts and collatorFactory code.
    Signed-off-by: Kaushalendra-Singh-007 <singhkush0307@gmail.com>

--- workspaces/confluence/plugins/search-backend-module-confluence-collator/config.d.ts
+++ workspaces/confluence/plugins/search-backend-module-confluence-collator/config.d.ts
@@ -30,70 +30,72 @@ export interface Config {
     };
   };
   confluence?: {
-    /**
-     * The base URL for accessing the Confluence API
-     * Typically: https://{org-name}.atlassian.net/wiki
-     */
-    baseUrl: string;
-    /**
-     * Confluence API credentials
-     */
-    auth: {
+    [instanceKey: string]: {
       /**
-       * Authentication method - basic, bearer, or userpass
+       * The base URL for accessing the Confluence API
+       * Typically: https://{org-name}.atlassian.net/wiki
        */
-      type: 'basic' | 'bearer' | 'userpass';
+      baseUrl: string;
       /**
-       * Confluence bearer authentication token with `Read` permissions, only required if type is set to 'basic' or 'bearer'.
-       * Reference the Confluence documentation to generate an API token:
-       * https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
-       * @visibility secret
+       * Confluence API credentials
        */
-      token?: string;
+      auth: {
+        /**
+         * Authentication method - basic, bearer, or userpass
+         */
+        type: 'basic' | 'bearer' | 'userpass';
+        /**
+         * Confluence bearer authentication token with `Read` permissions, only required if type is set to 'basic' or 'bearer'.
+         * Reference the Confluence documentation to generate an API token:
+         * https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+         * @visibility secret
+         */
+        token?: string;
+        /**
+         * Email associated with the token, only required if type is set to 'basic'.
+         * @visibility secret
+         */
+        email?: string;
+        /**
+         * Confluence basic authentication username, only required if type is set to 'userpass'.
+         * While Confluence supports BASIC authentication, using an API token is preferred.
+         * See: https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+         */
+        username?: string;
+        /**
+         * Confluence basic authentication password, only required if type is set to 'userpass'.
+         * While Confluence supports BASIC authentication, using an API token is preferred.
+         * See: https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+         * @visibility secret
+         */
+        password?: string;
+      };
       /**
-       * Email associated with the token, only required if type is set to 'basic'.
-       * @visibility secret
+       * Array of Confluence spaces to index. If omitted, all spaces will be indexed.
+       * See: https://support.atlassian.com/confluence-cloud/docs/use-spaces-to-organize-your-work/
        */
-      email?: string;
+      spaces?: string[];
       /**
-       * Confluence basic authentication username, only required if type is set to 'userpass'.
-       * While Confluence supports BASIC authentication, using an API token is preferred.
-       * See: https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+       * CQL query to select the pages to index. It is combined via 'AND' with spaces parameter above when finding documents.
+       * Reference the Confluence documentation for information about CQL syntax:
+       * https://developer.atlassian.com/server/confluence/advanced-searching-using-cql/
        */
-      username?: string;
+      query?: string;
       /**
-       * Confluence basic authentication password, only required if type is set to 'userpass'.
-       * While Confluence supports BASIC authentication, using an API token is preferred.
-       * See: https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
-       * @visibility secret
+       * An abstract value that controls the concurrency level of the
+       * collation process. Increasing this value will both increase the
+       * number of entities fetched at a time from the catalog, as well as how
+       * many things are being processed concurrently.
+       *
+       * Defaults to `15`.
+       * @deprecated Use `maxRequestsPerSecond` instead.
        */
-      password?: string;
+      parallelismLimit?: number;
+      /**
+       * The maximum number of requests per second to make to the Confluence API.
+       * @visibility backend
+       */
+      maxRequestsPerSecond?: number;
     };
-    /**
-     * Array of Confluence spaces to index. If omitted, all spaces will be indexed.
-     * See: https://support.atlassian.com/confluence-cloud/docs/use-spaces-to-organize-your-work/
-     */
-    spaces?: string[];
-    /**
-     * CQL query to select the pages to index. It is combined via 'AND' with spaces parameter above when finding documents.
-     * Reference the Confluence documentation for information about CQL syntax:
-     * https://developer.atlassian.com/server/confluence/advanced-searching-using-cql/
-     */
-    query?: string;
-    /**
-     * An abstract value that controls the concurrency level of the
-     * collation process. Increasing this value will both increase the
-     * number of entities fetched at a time from the catalog, as well as how
-     * many things are being processed concurrently.
-     *
-     * Defaults to `15`.
-     * @deprecated Use `maxRequestsPerSecond` instead.
-     */
-    parallelismLimit?: number;
-    /**
-     * The maximum number of requests per second to make to the Confluence API.
-     * @visibility backend
-     */
-    maxRequestsPerSecond?: number;
   };
 }
