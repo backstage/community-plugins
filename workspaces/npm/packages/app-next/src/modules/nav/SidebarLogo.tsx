bb724ca844c6e811ec16b83b3ca44067a4877eda
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-10-15 01:45:33 +0200

    npm - version:bump to v1.44.0 (#5748)
    * v1.44.0 version bump
    
    * chore(npm): add missing @backstage/ui css and update app-next
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    
    ---------
    
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Christoph Jerolimov <jerolimov+git@redhat.com>

---
+++ workspaces/npm/packages/app-next/src/modules/nav/SidebarLogo.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  Link,
+  sidebarConfig,
+  useSidebarOpenState,
+} from '@backstage/core-components';
+import { makeStyles } from '@material-ui/core';
+import { LogoFull } from './LogoFull';
+import { LogoIcon } from './LogoIcon';
+
+const useSidebarLogoStyles = makeStyles({
+  root: {
+    width: sidebarConfig.drawerWidthClosed,
+    height: 3 * sidebarConfig.logoHeight,
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    marginBottom: -14,
+  },
+  link: {
+    width: sidebarConfig.drawerWidthClosed,
+    marginLeft: 24,
+  },
+});
+
+export const SidebarLogo = () => {
+  const classes = useSidebarLogoStyles();
+  const { isOpen } = useSidebarOpenState();
+
+  return (
+    <div className={classes.root}>
+      <Link to="/" underline="none" className={classes.link} aria-label="Home">
+        {isOpen ? <LogoFull /> : <LogoIcon />}
+      </Link>
+    </div>
+  );
+};
