fb0214a0ad9a15c5c6ab827cba746e22b3fb5742
Ayush More <ayushmore42595@gmail.com>
2025-10-17 17:03:36 +0000

    fix: update Jenkins plugin
    Signed-off-by: Ayush More <ayushmore42595@gmail.com>

--- workspaces/jenkins/plugins/scaffolder-backend-module-jenkins/src/actions/job/build.ts
+++ workspaces/jenkins/plugins/scaffolder-backend-module-jenkins/src/actions/job/build.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Backstage Authors
+ * Copyright 2025 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,29 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
 import Jenkins from 'jenkins';
-import { z } from 'zod';
-
 /**
- * Create a scaffolder action to trigger a Jenkins job.
+ *
+ * This buildJob function, creates a template action for running a Jenkins job
+ *
+ * @param jenkins - The client to interact with jenkins instance
+ * @returns Empty response, in case of error an exception will be thrown by jenkins client
  */
 export function buildJob(jenkins: Jenkins) {
   return createTemplateAction({
     id: 'jenkins:job:build',
     description: 'Run an existing Jenkins job given its name',
     schema: {
-      input: z.object({
-        jobName: z.string().describe('Name of the Jenkins job to run'),
-        jobParameters: z.record(z.any()).optional(),
-      }),
+      input: z =>
+        z.object({
+          jobName: z.string().describe('Name of the Jenkins job to run'),
+          jobParameters: z.record(z.any()).optional(),
+          delay: z.number().optional(),
+          token: z.string().optional(),
+        }),
     },
+
     async handler(ctx) {
       ctx.logger.info(`Starting Jenkins job: ${ctx.input.jobName}`);
-
       await jenkins.job.build(ctx.input.jobName, ctx.input.jobParameters);
-
       ctx.logger.info('Job started successfully!');
     },
   });
