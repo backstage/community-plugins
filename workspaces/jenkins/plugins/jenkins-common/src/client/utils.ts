ca4ece2ccc34e7e3d9fa64f4682413fee513a05d
Bryan Ramos <bramos@redhat.com>
2025-10-20 12:36:09 -0400

    Add function docs to utility functions
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- workspaces/jenkins/plugins/jenkins-common/src/client/utils.ts
+++ workspaces/jenkins/plugins/jenkins-common/src/client/utils.ts
@@ -15,6 +15,13 @@
  */
 import { type Response } from 'node-fetch';
 
+/**
+ * Copies the {@link URL} object passed, appends query params and returns the resulting {@link URL}.
+ *
+ * @param u The {@link URL} object that will be copied
+ * @param q The {@link Record<string, string | number | undefined>} that stores query params
+ * @returns The resulting {@link URL} with query params.
+ */
 export function addQueryParams(
   u: URL,
   q: Record<string, string | number | undefined>,
@@ -31,6 +38,14 @@ export function addQueryParams(
   return dup;
 }
 
+/**
+ * Joins the base URL string with the specified path.
+ * Appends a `/` to the end of the `base` if it doesn't already have it.
+ *
+ * @param base The base URL string
+ * @param path The path that appends to the base string
+ * @returns A string of the full URL
+ */
 export function joinUrl(base: string, path: string): string {
   let dupBase = base;
   if (!dupBase.endsWith('/')) {
@@ -39,14 +54,33 @@ export function joinUrl(base: string, path: string): string {
   return dupBase + path;
 }
 
+/**
+ * Utility function that removes the `/` from the start of a string if it exists.
+ *
+ * @param p The string to trim
+ * @returns The string without the leading `/`
+ */
 export function trimLeadingSlash(p: string): string {
   return p.startsWith('/') ? p.slice(1) : p;
 }
 
+/**
+ * Utility function that ensures that string ends with `/`,
+ *
+ * @param u The string to modify
+ * @returns The resulting string, ending with `/`
+ */
 export function ensureTrailingSlash(u: string): string {
   return u.endsWith('/') ? u : `${u}/`;
 }
 
+/**
+ * Utility function that safely extracts the text from a response.
+ * If the operation results in an error a default string is returned instead.
+ *
+ * @param res The {@link Response} containing the `Body.text`
+ * @returns The resulting `Body.text` value or a default string if the operation failed
+ */
 export async function safeExtractText(res: Response): Promise<string> {
   try {
     return await res.text();
