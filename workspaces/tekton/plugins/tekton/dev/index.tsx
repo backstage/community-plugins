547b88894a3585312f379d5aca37695fc29fe3b2
Backstage Service Account <74306126+backstage-service@users.noreply.github.com>
2025-10-27 20:16:21 +0100

    tekton - version:bump to v1.44.0 (#5761)
    * v1.44.0 version bump
    
    * Add UI CSS changes
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    * Dedupe yarn.lock
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    
    ---------
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Bryan Ramos <bramos@redhat.com>

--- workspaces/tekton/plugins/tekton/dev/index.tsx
+++ workspaces/tekton/plugins/tekton/dev/index.tsx
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import '@backstage/ui/css/styles.css';
 import { Entity } from '@backstage/catalog-model';
 import { Page, Header, TabbedLayout } from '@backstage/core-components';
 import { createDevApp } from '@backstage/dev-utils';
