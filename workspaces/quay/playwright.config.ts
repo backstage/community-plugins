13b8f779c5a18098b07a7f7e5c53e5d978a134d0
Jan Richter <jrichter@redhat.com>
2025-11-27 13:38:23 +0100

    test(quay): UI test update and a11y check
    Signed-off-by: Jan Richter <jrichter@redhat.com>

---
+++ workspaces/quay/playwright.config.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  webServer: process.env.PLAYWRIGHT_URL
+    ? []
+    : {
+        command: 'yarn start',
+        cwd: 'plugins/quay',
+        port: 3000,
+        reuseExistingServer: true,
+      },
+
+  retries: process.env.CI ? 2 : 0,
+
+  reporter: [['html', { open: 'never', outputFolder: 'e2e-test-report' }]],
+
+  use: {
+    baseURL: process.env.PLAYWRIGHT_URL ?? 'http://localhost:3000',
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+  },
+
+  outputDir: 'node_modules/.cache/e2e-test-results',
+
+  projects: [
+    {
+      testDir: 'plugins/quay/tests',
+      use: {
+        channel: 'chrome',
+      },
+    },
+  ],
+});
