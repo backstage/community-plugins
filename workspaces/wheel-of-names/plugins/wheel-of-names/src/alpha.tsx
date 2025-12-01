f70cce327052a4389de9cf98e6c9c9690a8a9246
Vinnie McGuinness <57795181+vinnie-jog-on@users.noreply.github.com>
2025-12-01 20:11:15 +0000

    Wheel of name new frontend support (#5916)
    * chore: enable new frontend on wheel-of-names plugin
    
    chore: enable new frontend on wheel-of-names plugin
    
    chore: enable new frontend on wheel-of-names plugin
    
    Signed-off-by: vinnie-jog-on <vinnie.mcguinness@intive.com>
    
    chore: enable new frontend on wheel-of-names plugin
    
    Signed-off-by: vinnie-jog-on <vinnie.mcguinness@intive.com>
    
    ran yarn fix
    
    Signed-off-by: vinnie-jog-on <vinnie.mcguinness@intive.com>
    Signed-off-by: Vinnie McGuinness <vinnie.mcguinness@intive.com>
    
    * chore: enable new frontend on wheel-of-names plugin
    
    Signed-off-by: vinnie-jog-on <vinnie.mcguinness@intive.com>
    Signed-off-by: Vinnie McGuinness <vinnie.mcguinness@intive.com>
    
    * Fix review comments in README
    
    Signed-off-by: Vinnie McGuinness <vinnie.mcguinness@intive.com>
    
    * Remove lint-staged script from package.json
    
    Signed-off-by: Vinnie McGuinness <57795181+vinnie-jog-on@users.noreply.github.com>
    
    * Update workspaces/wheel-of-names/plugins/wheel-of-names/package.json
    
    Co-authored-by: Christoph Jerolimov <jerolimov@gmail.com>
    Signed-off-by: Vinnie McGuinness <57795181+vinnie-jog-on@users.noreply.github.com>
    
    * Update workspaces/wheel-of-names/plugins/wheel-of-names/package.json
    
    Co-authored-by: Christoph Jerolimov <jerolimov@gmail.com>
    Signed-off-by: Vinnie McGuinness <57795181+vinnie-jog-on@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: vinnie-jog-on <vinnie.mcguinness@intive.com>
    Signed-off-by: Vinnie McGuinness <vinnie.mcguinness@intive.com>
    Signed-off-by: Vinnie McGuinness <57795181+vinnie-jog-on@users.noreply.github.com>
    Co-authored-by: Christoph Jerolimov <jerolimov@gmail.com>

---
+++ workspaces/wheel-of-names/plugins/wheel-of-names/src/alpha.tsx
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import {
+  createFrontendPlugin,
+  PageBlueprint,
+  type FrontendPlugin,
+} from '@backstage/frontend-plugin-api';
+import {
+  compatWrapper,
+  convertLegacyRouteRef,
+  convertLegacyRouteRefs,
+} from '@backstage/core-compat-api';
+import { rootRouteRef } from './routes';
+
+const wheelOfNamesPage = PageBlueprint.make({
+  params: {
+    // This is the path that was previously defined in the app code.
+    // It's labelled as the default one because it can be changed via configuration.
+    path: '/wheel-of-names',
+    // You can reuse the existing routeRef by wrapping it with convertLegacyRouteRef.
+    routeRef: convertLegacyRouteRef(rootRouteRef),
+    // these inputs usually match the props required by the component.
+    loader: () =>
+      import('./pages/WheelOfNamesPage').then(m =>
+        // The compatWrapper utility allows you to keep using @backstage/core-plugin-api in the
+        // implementation of the component and switch to @backstage/frontend-plugin-api later.
+        compatWrapper(<m.WheelOfNamesPage />),
+      ),
+  },
+});
+
+/**
+ * The Wheel of Names plugin instance for the new frontend system.
+ * @alpha
+ */
+const plugin: FrontendPlugin = createFrontendPlugin({
+  // The plugin ID is now provided as `pluginId` instead of `id`
+  pluginId: 'wheel-of-names',
+  // bind all the extensions to the plugin
+  extensions: [wheelOfNamesPage],
+  // convert old route refs to the new system
+  routes: convertLegacyRouteRefs({
+    root: rootRouteRef,
+  }),
+});
+
+export default plugin;
