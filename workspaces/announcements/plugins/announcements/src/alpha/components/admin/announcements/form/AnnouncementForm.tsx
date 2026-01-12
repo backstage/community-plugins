3b362be3344a7bc60ba3bb32f91e436acdf951e7
Kurt King <kurtaking@gmail.com>
2026-01-12 11:37:09 -0700

    New category select input field
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ workspaces/announcements/plugins/announcements/src/alpha/components/admin/announcements/form/AnnouncementForm.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState } from 'react';
+import { Container, Grid, Text } from '@backstage/ui';
+
+import { CategorySelectInput } from './CategorySelectInput';
+import { Category } from '@backstage-community/plugin-announcements-common';
+
+export const AnnouncementForm = () => {
+  const [category, setCategory] = useState<Category | undefined>(undefined);
+
+  return (
+    <Container>
+      <Text variant="title-small">Announcement Form</Text>
+
+      <Grid.Root>
+        <Grid.Item>
+          <Text>Selected Category: {category?.title ?? 'None'}</Text>
+        </Grid.Item>
+      </Grid.Root>
+
+      <CategorySelectInput
+        initialCategory={category}
+        setCategory={setCategory}
+      />
+    </Container>
+  );
+};
