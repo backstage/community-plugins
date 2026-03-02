42c05f7c0697cef9eb05a9905e8d34f7af98f78e
Debsmita Santra <debsmita.santra@gmail.com>
2026-03-02 20:39:43 +0530

    feat(rbac): export rbacPlugin as a default (#7880)
    Signed-off-by: Debsmita Santra <debsmita.santra@gmail.com>

---
+++ workspaces/rbac/.changeset/clever-mice-rhyme.md
@@ -0,0 +1,5 @@
+---
+'@backstage-community/plugin-rbac': patch
+---
+
+exported rbacPlugin as a default and other minor updates
--- workspaces/rbac/packages/app/src/modules/nav/Sidebar.tsx
+++ workspaces/rbac/packages/app/src/modules/nav/Sidebar.tsx
@@ -25,8 +25,6 @@ import { compatWrapper } from '@backstage/core-compat-api';
 import { Sidebar } from '@backstage/core-components';
 import { NavContentBlueprint } from '@backstage/plugin-app-react';
 import { SidebarLogo } from './SidebarLogo';
-import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
-import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { SidebarSearchModal } from '@backstage/plugin-search';
@@ -46,14 +44,6 @@ export const SidebarContent = NavContentBlueprint.make({
           </SidebarGroup>
           <SidebarDivider />
           <SidebarGroup label="Menu" icon={<MenuIcon />}>
-            {/* Global nav, not org-specific */}
-            <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
-            <SidebarItem
-              icon={CreateComponentIcon}
-              to="create"
-              text="Create..."
-            />
-            {/* End global nav */}
             <SidebarDivider />
             <SidebarScrollWrapper>
               {/* Items in this group will be scrollable if they run out of space */}
