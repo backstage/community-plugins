715cf7ab2f62e5d44d30822d607aded3cd4f77e3
Christoph Jerolimov <jerolimov+git@redhat.com>
2026-03-10 13:17:54 +0100

    fix(argocd): resolve deprecated warnings
    Signed-off-by: Christoph Jerolimov <jerolimov+git@redhat.com>

--- workspaces/argocd/packages/app/src/components/Root/Root.tsx
+++ workspaces/argocd/packages/app/src/components/Root/Root.tsx
@@ -26,7 +26,6 @@ import {
   SidebarSpace,
   useSidebarOpenState,
 } from '@backstage/core-components';
-import { IconComponent } from '@backstage/core-plugin-api';
 import { MyGroupsSidebarItem } from '@backstage/plugin-org';
 import { Administration } from '@backstage-community/plugin-rbac';
 import { SidebarSearchModal } from '@backstage/plugin-search';
@@ -85,31 +84,15 @@ export const Root = ({ children }: PropsWithChildren): JSX.Element => (
       <SidebarDivider />
       <SidebarGroup label="Menu" icon={<MenuIcon />}>
         {/* Global nav, not org-specific */}
-        <SidebarItem
-          icon={HomeIcon as IconComponent}
-          to="catalog"
-          text="Home"
-        />
+        <SidebarItem icon={HomeIcon} to="catalog" text="Home" />
         <MyGroupsSidebarItem
-          icon={GroupIcon as IconComponent}
+          icon={GroupIcon}
           singularTitle="My Group"
           pluralTitle="My Groups"
         />
-        <SidebarItem
-          icon={ExtensionIcon as IconComponent}
-          to="api-docs"
-          text="APIs"
-        />
-        <SidebarItem
-          icon={LibraryBooks as IconComponent}
-          to="docs"
-          text="Docs"
-        />
-        <SidebarItem
-          icon={CreateComponentIcon as IconComponent}
-          to="create"
-          text="Create..."
-        />
+        <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
+        <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
+        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
         <Administration />
         {/* End global nav */}
         <SidebarDivider />
