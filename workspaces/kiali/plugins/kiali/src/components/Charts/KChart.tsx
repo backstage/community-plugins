0125860dc79dcb6ef659a13e04ea1de45d587641
SAY-5 <SAY-5@users.noreply.github.com>
2026-04-15 20:38:49 -0700

    kiali: fix 'occured' -> 'occurred' in KChart error message
    Error message in workspaces/kiali/plugins/kiali/src/components/Charts/KChart.tsx line 244 read 'An error occured while fetching'. User-visible in UI. Fixed to 'occurred'. String-literal-only change.
    
    Signed-off-by: SAY-5 <SAY-5@users.noreply.github.com>

--- workspaces/kiali/plugins/kiali/src/components/Charts/KChart.tsx
+++ workspaces/kiali/plugins/kiali/src/components/Charts/KChart.tsx
@@ -241,7 +241,7 @@ export class KChart<T extends LineInfo> extends React.Component<
             </Icon>
           )}
           <EmptyStateBody className={emptyStyle}>
-            An error occured while fetching this metric:
+            An error occurred while fetching this metric:
             <p>
               <i>{this.props.chart.error}</i>
             </p>
