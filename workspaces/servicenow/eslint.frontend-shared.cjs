0aa5613c2478e1083a26e51b1e42e7dec4b2332f
Yi C <yicai@redhat.com>
2026-07-03 07:18:11 -0400

    chore: add MUI v4 ESLint guards and migrate Tekton off @mui/styles (#9726)
    * chore: added eslint rules to prevent MUIv4 import
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * eslint rule update
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * addressed review comments
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * fix minor ui issue
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * added MUIv5 restriction on plugins already in BUI
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    ---------
    
    Signed-off-by: Yi Cai <yicai@redhat.com>

---
+++ workspaces/servicenow/eslint.frontend-shared.cjs
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const materialUiMigrationEslintConfig = {
+  restrictedImports: [
+    {
+      name: '@material-ui/core',
+      message: 'Use @mui/material instead of Material UI v4.',
+    },
+    {
+      name: '@material-ui/icons',
+      message: 'Use @mui/icons-material instead of Material UI v4.',
+    },
+    {
+      name: '@material-ui/lab',
+      message: 'Use @mui/material or @mui/lab instead of Material UI v4.',
+    },
+    {
+      name: '@material-ui/styles',
+      message: 'Use @mui/material (sx/styled) instead of Material UI v4.',
+    },
+    {
+      name: '@mui/styles',
+      message:
+        'Use @mui/material (sx/styled) instead of @mui/styles (legacy JSS styling).',
+    },
+  ],
+  restrictedImportPatterns: ['@material-ui/*'],
+};
+
+/**
+ * ESLint config for frontend packages in this workspace (MUI v4 migration guards).
+ */
+module.exports = function createEslintConfig(packageDir) {
+  return require('@backstage/cli/config/eslint-factory')(
+    packageDir,
+    materialUiMigrationEslintConfig,
+  );
+};
