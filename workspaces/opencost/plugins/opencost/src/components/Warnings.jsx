674c82822043a123ae098777c8e22fa7adf2997f
AmbrishRamachandiran <ambrish.r@infosys.com>
2026-08-01 01:30:46 +0530

    Migrated Opencost Plugin MUI to BUI
    Signed-off-by: AmbrishRamachandiran <ambrish.r@infosys.com>

--- workspaces/opencost/plugins/opencost/src/components/Warnings.jsx
+++ workspaces/opencost/plugins/opencost/src/components/Warnings.jsx
@@ -14,38 +14,37 @@
  * limitations under the License.
  */
 
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Paper from '@material-ui/core/Paper';
-import WarningIcon from '@material-ui/icons/Warning';
-
-const useStyles = makeStyles({
-  root: {},
-});
+import { Card, CardBody } from '@backstage/ui';
+import { RiAlertLine } from '@remixicon/react';
+import styles from './Warnings.module.css';
 
 const Warnings = ({ warnings }) => {
-  const classes = useStyles();
-
   if (!warnings || warnings.length === 0) {
     return null;
   }
 
   return (
-    <Paper className={classes.root}>
-      <List>
-        {warnings.map((warn, i) => (
-          <ListItem key={i}>
-            <ListItemIcon>
-              <WarningIcon />
-            </ListItemIcon>
-            <ListItemText primary={warn.primary} secondary={warn.secondary} />
-          </ListItem>
-        ))}
-      </List>
-    </Paper>
+    <Card>
+      <CardBody>
+        <ul className={styles.list}>
+          {warnings.map((warn, i) => (
+            <li key={i} className={styles.listItem}>
+              <div className={styles.listItemIcon}>
+                <RiAlertLine size={20} />
+              </div>
+              <div className={styles.listItemText}>
+                <div className={styles.listItemPrimary}>{warn.primary}</div>
+                {warn.secondary && (
+                  <div className={styles.listItemSecondary}>
+                    {warn.secondary}
+                  </div>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </CardBody>
+    </Card>
   );
 };
 
