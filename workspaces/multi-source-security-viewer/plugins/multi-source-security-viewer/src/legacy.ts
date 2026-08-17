0c13f0582ced402de0b3004048fda2ffdbca9e61
Eswaraiahsapram <esapram@redhat.com>
2026-08-17 16:59:19 +0530

    added back patternfly css
    Signed-off-by: Eswaraiahsapram <esapram@redhat.com>

--- workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/legacy.ts
+++ workspaces/multi-source-security-viewer/plugins/multi-source-security-viewer/src/legacy.ts
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/react-core/dist/styles/base-no-reset.css';
+
 import {
   configApiRef,
   createApiFactory,
